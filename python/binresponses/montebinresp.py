f12fb983b2cc91f255dd80714b7cbdc15f5907ca
Scott Ransom <sransom@nrao.edu>
1999-07-27 20:25:36 +0000

    Bug fixes.
    git-svn-id: file://localhost/data1/git/presto_svn/trunk@32 5fac236c-a0d8-0310-9745-ef116914eda1

--- python/binresponses/montebinresp.py
+++ python/binresponses/montebinresp.py
@@ -1,7 +1,8 @@
-import string, random, sys, cPickle
+from time import clock
 from math import *
 from Numeric import *
 from miscutils import *
+from Statistics import *
 from presto import *
 from orbitstuff import *
 
@@ -9,17 +10,19 @@ from orbitstuff import *
 parallel = 0          # True or false
 showplots = 1         # True or false
 debugout = 1          # True or false
-outfilenm = '/home/ransom/montebinresp_saves'
-orbsperpt = 100       # Number of orbits to test
+#outfiledir = '/tmp/scratch'
+outfiledir = '/home/ransom'
+outfilenm = 'montebinresp'
+orbsperpt = 100       # Number of orbits to average per point
 pmass = 1.35          # Pulsar mass in solar masses
 cmass = {'WD': 0.3, 'NS': 1.35, 'BH': 10.0}  # Companion masses to use
 ecc = {'WD': 0.0, 'NS': 0.6, 'BH': 0.6}      # Eccentricities to use
 
 # Simulation parameters
-numTbyPb = 100        # The number of points along the x axis
+numTbyPb = 50         # The number of points along the x axis
 minTbyPb = 0.01       # Minimum Obs Time / Orbital Period
 maxTbyPb = 10.0       # Maximum Obs Time / Orbital Period
-numppsr = 100         # The number of points along the y axis
+numppsr = 50          # The number of points along the y axis
 minppsr = 0.0005      # Minimum pulsar period (s)
 maxppsr = 5.0         # Maximum pulsar period (s)
 ctype = 'WD'          # The type of binary companion: 'WD', 'NS', or 'BH'
@@ -58,7 +61,19 @@ def estimate_rz(psr, T, eo=0.0):
     r = T / p_from_e(e, psr)
     return (average(r), average(z))
     
-outfilenm = outfilenm+'_'+searchtype+'.txt'
+# Figure out our environment 
+if showplots:
+    import Pgplot
+if parallel:
+    import mpi
+    from mpihelp import *
+    myid = mpi.comm_rank()
+    numprocs = mpi.comm_size()
+    outfilenm = outfiledir+'/'+outfilenm+`myid`+'_'+searchtype+'.out'
+else:
+    myid = 0
+    numprocs = 1
+    outfilenm = outfiledir+'/'+outfilenm+'_'+searchtype+'.out'
 
 # Calculate the values of our X and Y axis points
 logTbyPb = span(log(minTbyPb), log(maxTbyPb), numTbyPb)
@@ -70,18 +85,6 @@ ppsr = exp(logppsr)
 # (i.e. fpsr = 1 / ppsr = Nyquist freq / 2)
 N = 4 / ppsr
 T = N * dt
-
-# Figure out our environment 
-if showplots:
-    import Pgplot
-if parallel:
-    import mpi
-    from mpihelp import *
-    myid = mpi.comm_rank()
-    numprocs = mpi.comm_size()
-else:
-    myid = 0
-    numprocs = 1
 
 # Open a file to save each orbit calculation
 file = open(outfilenm,'w')
@@ -98,66 +101,65 @@ for x in range(numTbyPb):
         # Each processor calculates its own point
         if not (y % numprocs == myid):  continue
         else:
-            avgpow = 0.0
-            if searchtype == 'ffdot':
-                if Pb > maxTbyPb_ffdot:
-                    print `x`+'  '+`y`+'  '+`TbyPb[x]`+'  ',
-                    print `ppsr[y]`+'  '+`avgpow`
-                    file.write(`x`+'  '+`y`+'  '+`TbyPb[x]`+'  '+
-                               `ppsr[y]`+'  '+`avgpow`)
-                    continue
-            elif searchtype == 'sideband':
-                if Pb < minTbyPb_sideband:
-                    print `x`+'  '+`y`+'  '+`TbyPb[x]`+'  ',
-                    print `ppsr[y]`+'  '+`avgpow`
-                    file.write(`x`+'  '+`y`+'  '+`TbyPb[x]`+'  '+
-                               `ppsr[y]`+'  '+`avgpow`)
-                    continue
-            # Loop over the number of tries per point
-            for ct in range(orbsperpt):
-                if (eb == 0.0):
-                    wb, tp = 0.0, 0.0
-                else:
-                    (orbf, orbi)  = modf(ct / sqrt(orbsperpt))
-                    orbi = orbi / sqrt(orbsperpt)
-                    wb, tp = orbf * 180.0, Pb * orbi
-                psr = psrparams_from_list([ppsr[y], Pb, xb, eb, wb, tp])
-                print numTbyPb, psr.p, psr.orb.p, psr.orb.x
-                psr_halfwidth = bin_resp_halfwidth(psr.p, psr.orb)
-                psr_numbins = 2 * numbetween * psr_halfwidth
-                psr_resp = gen_bin_response(0.0, 1, psr.p, T[x], psr.orb, \
-                                            psr_numbins)
-                if searchtype == 'ffdot':
-                    # The following places the nominative psr freq
-                    # in bin # ~len(searchdata) / 2
-                    search = zeros(next2_to_n(psr_numbins * 2), 'F')
-                    lo = (len(search) - len(psr_resp)) / 2
-                    hi = lo + len(psr_resp)
-                    search[lo:hi] = psr_resp
-                    eo = keplars_eqn(psr.orb.t, psr.orb.p, psr.orb.e, 1.0e-14)
-                    (tryr, tryz) = estimate_fdot(psr, T[x], eo)
-                    tryr = T[x] / psr.p - tryr + len(searchdata) / 2.0
-                    nrz = 201
-                    ffd = ffdot_plane(search, tryr, 0.5, nrz, tryz, 2.0, nrz)
-                    maxarg = argmax(spectralpower(ffd.flat))
-                    [maxpow, rmax, zmax, rd] = maximize_rz(search,
-                                                           maxarg % nrz,
-                                                           maxarg / nrz)
-                    avgpow = avgpow + maxpow
+            pows = zeros(orbsperpt, 'd')
+            if ((searchtype == 'ffdot' and
+                 TbyPb[x] < maxTbyPb_ffdot) or
+                (searchtype == 'sideband' and
+                 TbyPb[x] > minTbyPb_sideband) or
+                (searchtype == 'shortffts')):
+                # Loop over the number of tries per point
+                for ct in range(orbsperpt):
+                    stim = clock()
+                    if (eb == 0.0):
+                        wb, tp = 0.0, 0.0
+                    else:
+                        (orbf, orbi)  = modf(ct / sqrt(orbsperpt))
+                        orbi = orbi / sqrt(orbsperpt)
+                        wb, tp = orbf * 180.0, Pb * orbi
+                    psr = psrparams_from_list([ppsr[y], Pb, xb,
+                                               eb, wb, tp])
+                    psr_numbins = 2 * bin_resp_halfwidth(psr.p, psr.orb)
+                    psr_resp = gen_bin_response(0.0, 1, psr.p,
+                                                T[x], psr.orb,
+                                                psr_numbins)
+                    if searchtype == 'ffdot':
+                        # The following places the nominative psr freq
+                        # approx in bin len(data)/2
+                        data = zeros(next2_to_n(psr_numbins * 2), 'F')
+                        lo = (len(data) - len(psr_resp)) / 2
+                        hi = lo + len(psr_resp)
+                        data[lo:hi] = array(psr_resp, copy=1)
+                        eo = keplars_eqn(psr.orb.t, psr.orb.p,
+                                         psr.orb.e, 1.0e-14)
+                        (tryr, tryz) = estimate_rz(psr, T[x], eo)
+                        tryr = T[x] / psr.p - tryr + len(data) / 2.0
+                        width = 201
+                        print psr_numbins, TbyPb[x], ppsr[y], eo, len(data),
+                        print tryr, tryz,
+                        ffd = ffdot_plane(data, tryr, 0.5, width,
+                                          tryz, 2.0, width)
+                        maxarg = argmax(spectralpower(ffd.flat))
+                        peakr = ((maxarg % width) * 0.5 +
+                                 int(tryr - (width * 0.5) / 2))
+                        peakz = ((maxarg / width) * 2.0 +
+                                 int(tryz - (width * 2.0) / 2))
+                        print peakr, peakz
+                        [pows[ct], rmax, zmax, rd] = \
+                                   maximize_rz(data, peakr, peakz, norm=1.0)
+                    elif searchtype == 'sideband':
+                        psr_pows = spectralpower(psr_resp)
+                        data = zeros(next2_to_n(psr_numbins * 2), 'd')
+                        data[0:psr_numbins] = psr_pows
                     if debugout:
                         print `x`+'  '+`y`+'  '+`TbyPb[x]`+'  ',
-                        print `ppsr[y]`+'  '+`maxpow`
-                        file.write(`x`+'  '+`y`+'  '+`TbyPb[x]`+'  '+
-                                   `ppsr[y]`+'  '+`maxpow`)                    
-                elif searchtype == 'sideband':
-                    psr_pows = spectralpower(psr_resp)
-                    search = zeros(next2_to_n(psr_numbins * 2), 'd')
-                    search[0:psr_numbins] = psr_pows
-            avgpow = avgpow / orbsperpt
+                        print `ppsr[y]`+'  '+`pows[ct]`
+                    tim = clock() - stim
+                    if debugout:
+                        print 'Time for this point was ',tim, ' s.'
             print `x`+'  '+`y`+'  '+`TbyPb[x]`+'  ',
-            print `ppsr[y]`+'  '+`avgpow`
-            file.write(`x`+'  '+`y`+'  '+`TbyPb[x]`+'  '+
-                       `ppsr[y]`+'  '+`avgpow`)                    
-            # Save our most recent orbit and search information
-#            cPickle.dump(widths[i], file, 1)
+            print `ppsr[y]`+'  '+`average(pows)`+'  ',
+            print `max(pows)`+'  '+`min(pows)`
+            file.write(`x`+'  '+`y`+'  '+`TbyPb[x]`+'  ')
+            file.write(`ppsr[y]`+'  '+`average(pows)`+'  ')
+            file.write(`max(pows)`+'  '+`min(pows)`+'\n')
 file.close()
