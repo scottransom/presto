13041d3f02989972db85fa97dc5f2672addae1cc
Scott Ransom <sransom@nrao.edu>
1999-08-11 21:26:46 +0000

    Should be usable.  Writes two picked arrays to the output file.
    git-svn-id: file://localhost/data1/git/presto_svn/trunk@68 5fac236c-a0d8-0310-9745-ef116914eda1

--- python/binresponses/monte_short.py
+++ python/binresponses/monte_short.py
@@ -4,48 +4,35 @@ from Numeric import *
 from presto import *
 from miscutils import *
 from Statistics import *
+from cPickle import *
+import Pgplot
 
 # Some admin variables
-parallel = 0          # True or false
-showplots = 1         # True or false
-debugout = 1          # True or false
+showplots = 0         # True or false
+showsumplots = 0      # True or false
+debugout = 0          # True or false
 outfiledir = '/home/ransom'
-outfilenm = 'monte'
+outfilenm = 'cpickle'
 pmass = 1.35                                 # Pulsar mass in solar masses
 cmass = {'WD': 0.3, 'NS': 1.35, 'BH': 10.0}  # Companion masses to use
 ecc = {'WD': 0.0, 'NS': 0.6, 'BH': 0.6}      # Eccentricities to use
-orbsperpt = {'WD': 20, 'NS': 100, 'BH': 100} # # of orbits to avg per pt
+orbsperpt = {'WD': 20, 'NS': 20, 'BH': 20}   # # of orbits to avg per pt
 ppsr = [0.002, 0.02, 0.2, 2.0]               # Pulsar periods to test
 
 # Simulation parameters
-numTbyPb = 100        # The number of points along the x axis
-minTbyPb = 0.01       # Minimum Obs Time / Orbital Period
-maxTbyPb = 10.0       # Maximum Obs Time / Orbital Period
-ctype = 'BH'          # The type of binary companion: 'WD', 'NS', or 'BH'
-Pb = 7200.0           # Orbital period in seconds
-dt = 0.0001           # The duration of each data sample (s)
-searchtype = 'sideband'  # One of 'ffdot', 'sideband', 'shortffts'
-fftlen_shortffts = 0.05
+ctype = 'WD'             # The type of binary companion: 'WD', 'NS', or 'BH'
+Pb = 7200.0              # Orbital period in seconds
+dt = 0.0001              # The duration of each data sample (s)
+searchtype = 'short'     # One of 'ffdot', 'sideband', 'short'
+Tfft = 60.0              # Length of FFTs in seconds (must evenly divide Pb)
+numbetween = 2
 
 ##################################################
 # You shouldn't need to edit anyting below here. #
 ##################################################
 
-# Figure out our environment 
-if showplots:
-    import Pgplot
-if parallel:
-    import mpi
-    from mpihelp import *
-    myid = mpi.comm_rank()
-    numprocs = mpi.comm_size()
-    outfilenm = (outfiledir+'/'+outfilenm+`myid`+
-                 '_'+searchtype+'_'+ctype+'.out')
-else:
-    myid = 0
-    numprocs = 1
-    outfilenm = (outfiledir+'/'+outfilenm+
-                 '_'+searchtype+'_'+ctype+'.out')
+outfilenm = (outfiledir+'/'+outfilenm+
+             '_'+searchtype+`Tfft`+'_'+ctype+'.out')
 
 def psrparams_from_list(pplist):
     psr = psrparams()
@@ -57,64 +44,61 @@ def psrparams_from_list(pplist):
     psr.orb.t = pplist[5]
     return psr
 
-def predict_mini_r(fftlen, Pb, T):
-    nyquist = fftlen / 2
-    r = fftlen * Pb / T
-    if (r > nyquist): rpred = alias(r, nyquist)
-    else: rpred = r
-    return rpred
-
 ####################################################################
-
-# Calculate the values of our X and Y axis points
-logTbyPb = span(log(minTbyPb), log(maxTbyPb), numTbyPb)
-TbyPb = exp(logTbyPb)
 
 # Open a file to save each orbit calculation
 file = open(outfilenm,'w')
 
-# The Simulation loops
+numffts = int(Pb / Tfft)
+TbyPb = (arange(numffts, typecode='d')+1.0)/numffts
+xb = asini_c(Pb, mass_funct2(pmass, cmass[ctype], pi / 3.0))
 
-# Loop over T / Porb
-for x in range(numTbyPb):
-    T = Pb * TbyPb[x]
-    xb = asini_c(Pb, mass_funct2(pmass, cmass[ctype], pi / 3.0))
-    eb = ecc[ctype]
-    # Loop over ppsr
-    for y in range(len(ppsr)):
-        # Each processor calculates its own point
-        if not (y % numprocs == myid):  continue
-        else:
-            pows = zeros(orbsperpt[ctype], 'd')
-            stim = clock()
-            # Loop over the number of tries per point
-            for ct in range(orbsperpt[ctype]):
-                if (eb == 0.0):
-                    wb, tp = 0.0, ct * Pb / orbsperpt[ctype]
-                else:
-                    (orbf, orbi)  = modf(ct / sqrt(orbsperpt[ctype]))
-                    orbi = orbi / sqrt(orbsperpt[ctype])
-                    wb, tp = orbf * 180.0, Pb * orbi
-                if debugout:
-                    print 'T = '+`T`+'  ppsr = '+`ppsr[y]`+\
-                          ' Pb = '+`Pb`+' xb = '+`xb`+' eb = '+\
-                          `eb`+' wb = '+`wb`+' tp = '+`tp`
-                psr = psrparams_from_list([ppsr[y], Pb, xb, eb, wb, tp])
-                psr_numbins = 2 * bin_resp_halfwidth(psr.p, T, psr.orb)
-                psr_resp = gen_bin_response(0.0, 1, psr.p, T, psr.orb,
-                                            psr_numbins)
-                if showplots:
-                    print "The raw response:"
-                    Pgplot.plotxy(spectralpower(psr_resp))
-                    Pgplot.closeplot()
-                if debugout:
-                    print `x`+'  '+`y`+'  '+`TbyPb[x]`+'  ',
-                    print `ppsr[y]`+'  '+`pows[ct]`
-            tim = clock() - stim
-            if debugout:
-                print 'Time for this point was ',tim, ' s.'
-            file.write('%5d  %9.6f  %8.6f  %11.9f  %11.9f  %11.9f\n' % \
-                       (y * numTbyPb + x, TbyPb[x], ppsr[y], \
-                        average(pows), max(pows), min(pows)))
-            file.flush()
+for pp in ppsr:
+    pows = zeros(orbsperpt[ctype], 'd')
+    stim = clock()
+    numbins = 0
+    for ct in range(orbsperpt[ctype]):
+        wb = ct * 180.0 / orbsperpt[ctype]
+        psr = psrparams_from_list([pp, Pb, xb, ecc[ctype], wb, 0.0])
+        tmpnumbins = 2 * numbetween * bin_resp_halfwidth(psr.p, Pb, psr.orb)
+        if tmpnumbins > numbins:  numbins = tmpnumbins
+    # Powers averaged over orb.t as a function of orb.w
+    pwrs_w = zeros((orbsperpt[ctype], numbins), Float32)
+    # Powers averaged over orb.w as a function of orb.t
+    pwrs_t = zeros((numffts, numbins), Float32)
+    for ct in range(orbsperpt[ctype]):
+        wb = ct * 180.0 / orbsperpt[ctype]
+        if debugout:  print 'wb = '+`wb`
+        psr = psrparams_from_list([pp, Pb, xb, ecc[ctype], wb, 0.0])
+        for i in range(numffts):
+            psr.orb.t = i * Tfft
+            tmppwrs = spectralpower(gen_bin_response(0.0, numbetween,
+                                                     psr.p, Tfft,
+                                                     psr.orb, numbins))
+            if debugout:  print '     tb = '+`psr.orb.t`+'  Max pow = '+\
+               `max(tmppwrs)`
+            if showplots:
+                Pgplot.plotxy(tmppwrs)
+                Pgplot.closeplot()
+            pwrs_w[ct] = pwrs_w[ct] + tmppwrs
+            pwrs_t[i] = pwrs_t[i] + tmppwrs
+        if showsumplots:
+            Pgplot.plotxy(pwrs_w[ct], title='power(w) averaged over orb.t')
+            Pgplot.closeplot()
+    for i in range(numffts):
+        if showsumplots:
+            Pgplot.plotxy(pwrs_t[i], title='power(t) averaged over orb.w')
+            Pgplot.closeplot()
+    pwrs_w = pwrs_w / numffts
+    pwrs_t = pwrs_t / orbsperpt[ctype]
+    if showsumplots:
+        Pgplot.plotxy(add.reduce(pwrs_w), title='power(w) averaged over orb.t')
+        Pgplot.closeplot()
+        Pgplot.plotxy(add.reduce(pwrs_t), title='power(t) averaged over orb.w')
+        Pgplot.closeplot()
+    tim = clock() - stim
+    if debugout:
+        print 'Time for this point was ',tim, ' s.'
+    dump(pwrs_w, file)
+    dump(pwrs_t, file)
 file.close()
