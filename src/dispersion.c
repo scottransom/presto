68d46db49117cbdd831f2f245eb64a41afb61934
Scott Ransom <sransom@nrao.edu>
1999-11-26 17:41:29 +0000

    Added smearing and tree_dedisp info functions.
    git-svn-id: file://localhost/data1/git/presto_svn/trunk@144 5fac236c-a0d8-0310-9745-ef116914eda1

--- src/dispersion.c
+++ src/dispersion.c
@@ -1,5 +1,32 @@
 #include "presto.h"
 
+double tree_max_dm(int nsubbands, double dt, double lofreq, double hifreq)
+/* Return the maximum Dispersion Measure (dm) in cm-3 pc, the  */
+/* tree de-dispersion technique can correct for given a sample */
+/* interval 'dt', the number of subbands 'nsubbands', and the  */
+/* low and high observation frequencies in MHz.                */
+{
+  if (lofreq==0.0 || hifreq==0.0)
+    return 0.0;
+  else
+    return 0.000241 * (nsubbands - 1) * dt / 
+      ((1.0 / (lofreq * lofreq)) - (1.0 / (hifreq * hifreq)));
+}
+
+
+double smearing_from_bw(double dm, double center_freq, double bandwidth)
+/* Return the smearing in seconds caused by dispersion, given  */
+/* a Dispersion Measure (dm) in cm-3 pc, the central frequency */
+/* and the bandwith of the observation in MHz.                 */
+{
+  if (center_freq==0.0)
+    return 0.0;
+  else
+    return dm * bandwidth / 
+      (0.0001205 * center_freq * center_freq * center_freq);
+}
+
+
 double delay_from_dm(double dm, double freq_emitted)
 /* Return the delay in seconds caused by dispersion, given  */
 /* a Dispersion Measure (dm) in cm-3 pc, and the emitted    */
