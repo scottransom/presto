519de3b3e7401f4ad4076009c6c7e51ed50c2d9c
Scott Ransom <sransom@nrao.edu>
2000-08-31 21:50:00 +0000

    Added dftfold files.
    git-svn-id: file://localhost/data1/git/presto_svn/trunk@347 5fac236c-a0d8-0310-9745-ef116914eda1

---
+++ include/dftfold.h
@@ -0,0 +1,23 @@
+#include "rawtype.h"
+
+typedef struct DFTVECTOR {
+  int n;            /* Number of data points in each vector segment   */
+  int numvect;      /* Number of vectors in the fold (vector length)  */
+  double dt;        /* Duration (s) of each data point                */
+  double r;         /* Fourier frequency folded                       */
+  double norm;      /* Normalization used on vector                   */
+  fcomplex *vector; /* Calculated DFT vector                          */
+} dftvector;
+
+void read_dftvector(dftvector *data, char *filename);
+/* Read a dftvector data structure from a binary file */
+
+void write_dftvector(dftvector *data, char *filename);
+/* Write a dftvector data structure to a binary file */
+
+void init_dftvector(dftvector *data, int n, int numvect, 
+		    double dt, double r, double norm);
+/* Initialize a dftvector and allocate its vector part */
+
+void free_dftvector(dftvector *data);
+/* Free the dynamically allocated vector in data */
