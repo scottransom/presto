83b086bc76061d27eb4d9e23e74feab242dcab53
Scott Ransom <sransom@nrao.edu>
2000-09-14 21:12:51 +0000

    Fits work OK now.  Many other small changes.
    git-svn-id: file://localhost/data1/git/presto_svn/trunk@356 5fac236c-a0d8-0310-9745-ef116914eda1

--- python/dftvectors.py
+++ python/dftvectors.py
@@ -1,7 +1,8 @@
 import umath
 import Numeric
-import Pgplot
-import copy
+from Pgplot import plotxy, closeplot
+from copy import copy
+from time import sleep
 from Multipack import leastsq
 
 def smooth_phases(phases):
@@ -25,18 +26,52 @@ def add_components(components):
    """
    return umath.add.accumulate(components)
 
+def p_to_f(p, pd, pdd=None):
+   """
+   p_to_f(p, pd, pdd=None):
+      Convert period, period derivative and period second
+      derivative to the equivalent frequency counterparts.
+      Will also convert from f to p.
+   """
+   f = 1.0 / p
+   fd = -pd / (p * p)
+   if (pdd==None):
+       return [f, fd]
+   else:
+       if (pdd==0.0):
+           fdd = 0.0
+       else:
+           fdd = 2.0 * pd * pd / (p**3.0) - pdd / (p * p)
+       return [f, fd, fdd]
+
 def vector_amplitude(vector):
    return umath.absolute(vector[-1])
 
 def vector_phase(vector):
    return umath.arctan2(vector[-1].imag, vector[-1].real)
 
-def rotate_vector(vector, angles):
-   return vector * umath.exp(complex(0.0, 1.0) * angles)   
+def rotate_components(components, angles):
+   return components * umath.exp(complex(0.0, 1.0) * angles)   
 
 def point_distances(vec1, vec2):
    return umath.sqrt((vec2.real-vec1.real)**2.0 +
                      (vec2.imag-vec1.imag)**2.0)
+
+def rzw_dist_model(przw, self):
+   przw = przw * Numeric.array([1.0, 1.0, 4.0, 20.0])
+   amplitudes = Numeric.ones(self.numvect, 'D')
+   phases = self.rzw_phases(przw)
+   corrvect = add_components(rotate_components(amplitudes, phases))
+   norm = self.amplitude() / vector_amplitude(corrvect)
+   weights = Numeric.arange(1.0, 0.0, -1.0/self.numvect)
+   return weights * point_distances(self.vector, norm * corrvect)
+
+def rzw_phase_model(przw, self):
+   przw = przw * Numeric.array([1.0, 1.0, 4.0, 20.0])
+   corrvect = add_components(self.rotate(-self.rzw_phases(przw)))
+   phases = umath.arctan2(corrvect.imag, corrvect.real)
+   weights = self.timefract
+   return weights * smooth_phases(phases)
 
 class dftvector:
    def __init__(self, filename=None):
@@ -56,11 +91,12 @@ class dftvector:
       self.dt = dblarr[2]
       self.r = dblarr[3]
       self.norm = dblarr[4]
+      self.T = dblarr[5]
       self.components = Numeric.zeros(self.numvect, 'D')
       self.timefract = (Numeric.arange(self.numvect) + 1.0) / self.numvect
       self.times = self.timefract * self.dt * self.n * self.numvect
       for ii in xrange(self.numvect):
-         index = 2 * ii + 5
+         index = 2 * ii + 6
          self.components[ii] = complex(dblarr[index], dblarr[index+1])
       self.vector = add_components(self.components)
    def rebin(self, factor):
@@ -75,10 +111,10 @@ class dftvector:
       if (factor == 0):
          self.n = self.orig_n
          self.numvect = self.orig_numvect
-         self.components = copy.copy(self.orig_components)
-         self.vector = copy.copy(self.orig_vector)
-         self.timefract = copy.copy(self.orig_timefract)
-         self.times = copy.copy(self.orig_times)
+         self.components = copy(self.orig_components)
+         self.vector = copy(self.orig_vector)
+         self.timefract = copy(self.orig_timefract)
+         self.times = copy(self.orig_times)
       elif not (factor * (self.numvect / factor) == self.numvect):
          print "'factor' in dftvector.rebin(factor) must evenly divide"
          print "   the number of vector components (numvect)."
@@ -88,16 +124,34 @@ class dftvector:
          except AttributeError:
             self.orig_n = self.n
             self.orig_numvect = self.numvect
-            self.orig_components = copy.copy(self.components)
-            self.orig_vector = copy.copy(self.vector)
-            self.orig_timefract = copy.copy(self.timefract)
-            self.orig_times = copy.copy(self.times)
-         self.n = self.n * factor
-         self.numvect = self.numvect / factor
-         self.components = rebin_components(self.components, factor)
-         self.vector = add_components(self.components)
-         self.timefract = (Numeric.arange(self.numvect) + 1.0) / self.numvect
-         self.times = self.timefract * self.dt * self.n * self.numvect
+            self.orig_components = copy(self.components)
+            self.orig_vector = copy(self.vector)
+            self.orig_timefract = copy(self.timefract)
+            self.orig_times = copy(self.times)
+         if (factor > 1):
+            self.n = self.n * factor
+            self.numvect = self.numvect / factor
+            self.components = rebin_components(self.components, factor)
+            self.vector = add_components(self.components)
+            self.timefract = (Numeric.arange(self.numvect) + 1.0) / self.numvect
+            self.times = self.timefract * self.dt * self.n * self.numvect
+   def mask(self, start, stop):
+      """
+      dftvector.mask(start, stop):
+           Set the dftvector.components that are located between the
+           fractional times (i.e. 0.0-1.0) 'start' and 'stop' to 0.0.
+      """
+      self.rebin(1)
+      beg = Numeric.searchsorted(self.timefract, start)
+      end = Numeric.searchsorted(self.timefract, stop)
+      self.components[beg:end] = complex(0.0, 0.0)
+      self.vector = add_components(self.components)
+   def unmask(self):
+      """
+      dftvector.unmask():
+           Remove all masks from the data.
+      """
+      self.rebin(0)
    def rotate(self, angles):
       """
       dftvector.rotate(angles):
@@ -136,60 +190,59 @@ class dftvector:
            f-dotdot (w).  'przw' is a list with 4 elements.
       """
       u = self.timefract
-      return (2.0 * umath.pi * u *
-              (przw[1] + u * (0.5 * przw[2] + przw[3] / 6.0 * u))) + przw[0]
-   def fitrzw(self, przw=None):
+      if (przw[3]==0.0):
+         if (przw[2]==0.0):
+            return 2.0 * umath.pi * u * przw[1] + przw[0]
+         else:
+            return 2.0 * umath.pi * u * (przw[1] + 0.5 * u * przw[2]) + przw[0]
+      else:
+         return (2.0 * umath.pi * u *
+                 (przw[1] + 0.5 * u * (przw[2] + u * przw[3] / 3.0))) + przw[0]
+   def rzwinfo(self, przw):
       """
-      dftvector.fitrzw():
+      dftvector.rzwinfo(self, przw):
+           Nicely print the conversions of the przw array.
+      """
+      modprzw = Numeric.asarray(copy(przw[1:]))
+      modprzw[0] = (self.r + modprzw[0])
+      [f, fd, fdd] = (modprzw / self.T**Numeric.array([1.0, 2.0, 3.0])).tolist()
+      [p, pd, pdd] = p_to_f(f, fd, fdd)
+      print ""
+      print "      Phase (rad) = %.3f" % (przw[0])
+      print "      Phase (deg) = %.3f" % (przw[0] * 180.0 / umath.pi)
+      print "     Fourier freq = %.3f" % (modprzw[0])
+      print "     Fourier fdot = %.5g" % (modprzw[1])
+      print "  Fourier fdotdot = %.5g" % (modprzw[2])
+      print "   Frequency (Hz) = %.9f" % (f)
+      print "      fdot (Hz/s) = %.5g" % (fd)
+      print " fdotdot (Hz/s^2) = %.5g" % (fdd)
+      print "       Period (s) = %.12f" % (p)
+      print "       pdot (s/s) = %.5g" % (pd)
+      print "  pdotdot (s/s^2) = %.5g" % (pdd)
+      print ""
+   def fitrzw(self, przw=None, funct=rzw_dist_model):
+      """
+      dftvector.fitrzw(przw=None, funct=rzw_dist_model):):
            Fit a model consisting of an arbitrary phase, Fourier
            frequency offset (dr), Fourier f-dot (z), and Fourier
            f-dotdot (w) to dftvector.  Return the best fitting
-           parameters in an array of [phase, dr, z, w].
+           parameters in an array of [phase, dr, z, w].  If 'przw'
+           is defined it is used as the starting guess for the
+           fit.  If 'funct' is defined that is the function that
+           is used during the minimization.
       """
-      def funct(przw, self):
-         x = Numeric.zeros(4, 'd')
-         x[0:3] = przw
-         corrvect = umath.add.accumulate(self.rotate(-self.rzw_phases(x)))
-         phases = umath.arctan2(corrvect.imag, corrvect.real)
-         #self.plotvector(corrvect)
-         #Pgplot.closeplot()
-         return smooth_phases(phases)
-         #weights = self.timefract
-         #Pgplot.plotxy(weights * (self.phases() - self.rzw_phases(x)))
-         #Pgplot.closeplot()
-         #return weights * (self.phases() - self.rzw_phases(x))
-      if not przw:
-         przw = [0.0, 0.0, 0.0]
+      if przw:
+         przw = Numeric.asarray(przw) / Numeric.array([1.0, 1.0, 4.0, 20.0])
+      else:
+         przw = Numeric.array([0.0, 0.0, 0.0, 0.0])
       retval = leastsq(funct, przw, args=(self))
       print retval[1]
-      x = Numeric.zeros(4, 'd')
-      x[0:3] = retval[0]
-      return x
-   def fitrzw_dist(self, aprzw=None):
+      return retval[0] * Numeric.array([1.0, 1.0, 4.0, 20.0])
+   def plotvector(self, vector, amp=None, color='red',
+                  device='/XWIN', step=0):
       """
-      dftvector.fitrzw_dist():
-           Fit a model consisting of an amplitude, phase, Fourier
-           frequency offset (dr), Fourier f-dot (z), and Fourier
-           f-dotdot (w) to dftvector.  Return the best fitting
-           parameters in an array of [amp, phase, dr, z, w].
-      """
-      def funct(aprzw, self):
-         amplitudes = Numeric.arange(self.numvect, typecode='d') / \
-                      self.numvect  * aprzw[0]
-         phases = self.rzw_phases(aprzw[1:])
-         corrvect = add_components(rotate_vector(amplitudes, phases))
-         #self.plotvector(corrvect)
-         #return weights * point_distances(self.vector, corrvect)
-         #weights = self.timefract**2.0
-         return point_distances(self.vector, corrvect)
-      if not aprzw:
-         aprzw = [0.5 * self.maxamplitude(), 0.0, 0.0, 0.0, 0.0]
-      retval = leastsq(funct, aprzw, args=(self))
-      print retval[1]
-      return retval[0]
-   def plotvector(self, vector, amp=None, color='red', device='/XWIN'):
-      """
-      dftvector.plotvector(vector, amp=None, color='red', device='/XWIN'):
+      dftvector.plotvector(vector, amp=None, color='red',
+                           device='/XWIN', step=0)
            Plot 'vector' in the complex plane.
       """
       x = Numeric.zeros(self.numvect+1, 'd')
@@ -199,59 +252,94 @@ class dftvector:
       if not amp:
          amp = 1.1 * umath.maximum(max(umath.absolute(x)),
                                    max(umath.absolute(y)))
-      Pgplot.plotxy(y, x, rangex=[-amp, amp], rangey=[-amp, amp],
-                    labx='Real Amplitude', laby='Imaginary Amplitude',
-                    color=color, aspect=1.0)
-   def plot(self, amp=None, color='white', device='/XWIN'):
+      if step:
+         for ii in xrange(self.numvect):
+            sleep(0.01)
+            plotxy(y[ii:ii+2], x[ii:ii+2],
+                   rangex=[-amp, amp], rangey=[-amp, amp],
+                   labx='Real Amplitude', laby='Imaginary Amplitude',
+                   color=color, aspect=1.0)
+      else:
+         plotxy(y, x, rangex=[-amp, amp], rangey=[-amp, amp],
+                labx='Real Amplitude', laby='Imaginary Amplitude',
+                color=color, aspect=1.0)
+   def plot(self, amp=None, color='white', device='/XWIN', step=0):
       """
-      dftvector.plot(amp=None, color='white', device='/XWIN'):
+      dftvector.plot(amp=None, color='white', device='/XWIN', step=0)
            Plot the dftvector summation.
       """
-      self.plotvector(self.vector, amp=amp, color=color, device=device)
+      self.plotvector(self.vector, amp=amp, color=color,
+                      device=device, step=step)
 
-#dv = dftvector("/raid/data/Ter5_98/Day1-2/Ter5_1-2_bary_DM236_3564593.380.dftvec")
-#print dv.maxamplitude()
-#print dv.amplitude()
-#dv.plot()
+x=1
+if (x==0):
+   dv = dftvector("/raid/data/Ter5_98/Day1-2/Ter5_1-2_bary_DM236_3564593.380.dftvec")
+   phs = 4.50
+   rfold = dv.r
+   rmeas = 3564593.40
+   z = -0.3
+   przw = [phs, (rmeas - 0.5 * z) - rfold, z, 0.0]
+elif (x==1):
+   dv = dftvector("/raid/data/NGC6544/NGC6544_DM134_bary_566514.000.dftvec")
+   phs = 2.63
+   rfold = dv.r
+   rmeas = 566511.93
+   z = 8.6
+   przw = [phs, (rmeas - 0.5 * z) - rfold, z, 0.0]
+else:
+   dv = dftvector("testz_1000000.000.dftvec")
+   phs = 3.88
+   rfold = dv.r
+   rmeas = 1000003.24
+   z = 5.8
+   przw = [phs, (rmeas - 0.5 * z) - rfold, z, 0.0]
+   dv.rebin(4)
 
-dv = dftvector("testz_1000000.000.dftvec")
-print dv.maxamplitude()
-print dv.amplitude()
-aprzw = [dv.amplitude(), 224.657*umath.pi/180.0, 0.345, 5.789, 0.0]
-print "aprzw =", aprzw
-phasecorr = -dv.rzw_phases(aprzw[1:])
-corrvect = umath.add.accumulate(dv.rotate(phasecorr))
-dv.plotvector(corrvect, color="red")
-## amplitudes = Numeric.arange(dv.numvect, typecode='d') / \
-##              dv.numvect * aprzw[0]
-## phases = dv.rzw_phases(aprzw[1:])
-## corrvect = add_components(rotate_vector(amplitudes, phases))
-## dv.plotvector(corrvect, color="yellow")
-
-aprzw = dv.fitrzw_dist()
-print "aprzw =", aprzw
-phasecorr = -dv.rzw_phases(aprzw[1:])
-corrvect = umath.add.accumulate(dv.rotate(phasecorr))
-dv.plotvector(corrvect, color="blue")
-dv.plot()
-Pgplot.closeplot()
-
-## dv = dftvector("/raid/data/NGC6544/NGC6544_DM134_bary_566514.000.dftvec")
-## przw = [0.0, -6.37, 8.6, 0.0]
-## print "przw =", przw
-## corrvect = add_components(dv.rotate(-dv.rzw_phases(przw)))
-## print "corrvect amp =", vector_amplitude(corrvect)
-## print "corrvect phs =", vector_phase(corrvect)
-## dv.plotvector(corrvect)
-## dv.plot()
-## Pgplot.closeplot()
-
-#dv = dftvector("/pulsar/data/parkes/Ter5_1_Part/Ter5_DM243_76375.000.dftvec")
-#print dv.maxamplitude()
-#print dv.amplitude()
-#dv.plot(amp=12)
-#przw = dv.fitrzw()
-#rot = dv.rzw_phases(przw)
-#corrvect = dv.rotate(-rot)
-#dv.plotvector(umath.add.accumulate(corrvect))
-#Pgplot.closeplot()
+if (1):
+   print "    Max amplitude =", dv.maxamplitude()
+   print "Current amplitude =", dv.amplitude()
+   dv.rzwinfo(przw)
+   phasecorr = dv.rzw_phases(przw)
+   corrvect = add_components(dv.rotate(-phasecorr))
+   dv.plotvector(corrvect, color="red")
+   amplitudes = Numeric.ones(dv.numvect, 'D')
+   corrvect = add_components(rotate_components(amplitudes, phasecorr))
+   norm = dv.amplitude() / vector_amplitude(corrvect)
+   dv.plotvector(norm * corrvect, color="blue")
+   dv.plot()
+   closeplot()
+   
+   przw = dv.fitrzw(przw)
+   dv.rzwinfo(przw)
+   phasecorr = dv.rzw_phases(przw)
+   corrvect = add_components(dv.rotate(-phasecorr))
+   dv.plotvector(corrvect, color="red")
+   amplitudes = Numeric.ones(dv.numvect, 'D')
+   corrvect = add_components(rotate_components(amplitudes, phasecorr))
+   norm = dv.amplitude() / vector_amplitude(corrvect)
+   dv.plotvector(norm * corrvect, color="blue")
+   dv.plot()
+   closeplot()
+   
+   przw = dv.fitrzw(przw, rzw_phase_model)
+   dv.rzwinfo(przw)
+   phasecorr = dv.rzw_phases(przw)
+   corrvect = add_components(dv.rotate(-phasecorr))
+   dv.plotvector(corrvect, color="red")
+   amplitudes = Numeric.ones(dv.numvect, 'D')
+   corrvect = add_components(rotate_components(amplitudes, phasecorr))
+   norm = dv.amplitude() / vector_amplitude(corrvect)
+   dv.plotvector(norm * corrvect, color="blue")
+   dv.plot()
+   closeplot()
+else:
+   dv = dftvector("/raid/data/Ter5_98/Day1-2/Ter5_1-2_bary_DM243_2600530.000.dftvec")
+   dv.plot(step=1)
+   closeplot()
+   plotxy(abs(dv.components), dv.timefract)
+   dv.mask(0.0, 0.33)
+   dv.mask(0.43, 0.5)
+   dv.mask(0.6, 1.01)
+   plotxy(abs(dv.components), dv.timefract, color='red')
+   closeplot()
+   dv.plot(color='red', step=1)
