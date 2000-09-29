8d54b2311f1d08ea2bfe5a6e163ae1124abe96f9
Scott Ransom <sransom@nrao.edu>
2000-09-29 21:45:31 +0000

    Added multifiles.c to CVS.  Done, but buggy...
    git-svn-id: file://localhost/data1/git/presto_svn/trunk@369 5fac236c-a0d8-0310-9745-ef116914eda1

---
+++ src/multifiles.c
@@ -0,0 +1,241 @@
+#include "multifiles.h"
+
+void print_multifile(multifile *mfile, int full)
+/* Print a multifile structure.  If 'full' is true, */
+/* print everything.  Used for debugging.           */     
+{
+  if (full){
+    int ii;
+    
+    printf("There are %d files (maxlen = %lld, mode = '%s'):\n", 
+	   mfile->numfiles, mfile->maxfilelen, mfile->mode);
+    for (ii=0; ii<mfile->numfiles; ii++){
+      printf("   %d:  '%s', len = %lld'\n", 
+	     ii, mfile->filenames[ii], mfile->filelens[ii]);
+    }
+  }
+  printf(" File = %d, Filepos = %lld, Fullpos = %lld, Length = %lld\n", 
+	 mfile->currentfile, mfile->currentpos, mfile->position, 
+	 mfile->length); 
+}
+
+multifile *fopen_multifile(int numfiles, char **filenames, char *mode, 
+			   long long maxlen)
+/* Open a multifile for use and return the multifile structure.  */
+/*  'numfiles' is the number of files in the multifile.          */
+/*  'filenames' is an array of the names of the component files  */
+/*  'mode' is the method of opening the file (binary is assumed) */
+/* 	"r" : read only, do not create (truncate) the files      */
+/*      "r+": read+write, do not create (truncate) the files     */
+/*      "w" : read+write, create (truncate) the files            */
+/*      "a" : read+write, create files or open at end of files   */
+/*  'maxlen' is the maximum length in bytes of each file.  This  */
+/*      number is only used if a file is opened fo writing.  The */
+/*      default value is DEFAULT_MAXLEN.  If you want to use     */
+/*      the default, simply set 'maxlen' to 0.                   */
+{
+  multifile *mfile=NULL;
+  int ii, append=0;
+
+  mfile->numfiles = numfiles;
+  mfile->currentfile = 0;
+  mfile->currentpos = 0;
+  mfile->length = 0;
+  mfile->position = 0;
+  if (maxlen > 0)
+    mfile->maxfilelen = maxlen;
+  else
+    mfile->maxfilelen = DEFAULT_MAXLEN;
+  mfile->filelens = malloc(numfiles * sizeof(int));
+  mfile->filenames = malloc(numfiles * sizeof(char *));
+  mfile->fileptrs = malloc(numfiles * sizeof(FILE *));
+  if (strncmp(mode, "r", 1)==0){
+    if (strncmp(mode, "r+", 2)==0){
+      strcpy(mfile->mode, "rb+");
+    } else {
+      strcpy(mfile->mode, "rb");
+    }
+  } else if (strncmp(mode, "w", 1)==0){
+    strcpy(mfile->mode, "wb");
+  } else if (strncmp(mode, "a", 1)==0){
+    strcpy(mfile->mode, "ab");
+    append = 1;
+  } else {
+    printf("\n'mode' = '%s' in open_multifile() is not valid.\n\n", 
+	   mode);
+    return NULL;
+  }
+  for (ii=0; ii<numfiles; ii++){
+    mfile->filenames[ii] = \
+      calloc(strlen(filenames[ii]+1), 1);
+    strcpy(mfile->filenames[ii], filenames[ii]);
+    mfile->fileptrs[ii] = \
+      chkfopen(filenames[ii], mfile->mode);
+    mfile->filelens[ii] = \
+      chkfilelen(mfile->fileptrs[ii], 1);
+    mfile->length += mfile->filelens[ii];
+  }
+  if (append){
+    mfile->currentfile = numfiles-1;
+    chkfileseek(mfile->fileptrs[mfile->currentfile], 
+		0, 1, SEEK_END);
+  }
+  return mfile;
+}
+
+int fclose_multifile(multifile *mfile)
+/* Close a multifile and free its resources. */
+{
+  int ii, flag=0;
+  
+  for (ii=0; ii<mfile->numfiles; ii++){
+    flag = fclose(mfile->fileptrs[ii]);
+    free(mfile->filenames[ii]);
+  }
+  free(mfile->filenames);
+  free(mfile->fileptrs);
+  free(mfile->filelens);
+  return flag;
+}
+
+int fread_multifile(void *data, size_t type, size_t number, 
+		    multifile *mfile)
+/* Read binary data from a multifile.                        */
+/*   'data' is an array of the proper type to store the data */
+/*   'type' is the size of each nugget of data to read       */
+/*   'number' is the number of nuggets to read               */
+/*   'mfile' is a pointer to a valid multifile structure     */
+{
+  int findex, readbytes, bytesread;
+  
+  findex = mfile->currentfile;
+  readbytes = number * type;
+  bytesread = chkfread((char *)data, 1, readbytes, 
+		       mfile->fileptrs[findex]);
+  mfile->currentpos += bytesread;
+  mfile->position += bytesread;
+  readbytes -= bytesread;
+  while (readbytes){
+    if (feof(mfile->fileptrs[findex])){
+      if (findex == mfile->numfiles-1){
+	return bytesread / type;
+      } else {
+	findex++;
+	mfile->currentfile++;
+	mfile->currentpos = 0;
+	bytesread = chkfread((char *)data + bytesread, 1, 
+			     readbytes, mfile->fileptrs[findex]);
+	mfile->currentpos += bytesread;
+	mfile->position += bytesread;
+	readbytes -= bytesread;
+      }
+    } else {
+      printf("\nRead error in read_multifile():\n");
+      printf("\tTried to read %d bytes, only read %d!\n\n",
+	     number * type, bytesread);
+      return bytesread / type;
+    }
+  }
+  return bytesread / type;
+}
+    
+int fwrite_multifile(void *data, size_t type, size_t number, 
+		     multifile *mfile)
+/* Write binary data to a multifile.                         */
+/*   'data' is an array of data to write                     */
+/*   'type' is the size of each nugget of data to write      */
+/*   'number' is the number of nuggets to write              */
+/*   'mfile' is a pointer to a valid multifile structure     */
+{
+  int findex, writebytes, byteswritten;
+  
+  findex = mfile->currentfile;
+  writebytes = number * type;
+  byteswritten = chkfwrite((char *)data, 1, writebytes, 
+			mfile->fileptrs[findex]);
+  mfile->currentpos += byteswritten;
+  if (mfile->currentpos > mfile->filelens[findex]){
+    mfile->length += (mfile->currentpos - 
+		      mfile->filelens[findex]);
+    mfile->filelens[findex] = mfile->currentpos;
+  }
+  mfile->position += byteswritten;
+  writebytes -= byteswritten;
+  while (writebytes){
+    if (mfile->currentpos==mfile->maxfilelen){
+      if (findex < mfile->numfiles-1){
+	findex++;
+	mfile->currentfile++;
+	mfile->currentpos = 0;
+	byteswritten = chkfwrite((char *)data + byteswritten, 1, 
+				 writebytes, mfile->fileptrs[findex]);
+	mfile->currentpos += byteswritten;
+	if (mfile->currentpos > mfile->filelens[findex]){
+	  mfile->length += (mfile->currentpos - 
+			    mfile->filelens[findex]);
+	  mfile->filelens[findex] = mfile->currentpos;
+	}
+	mfile->position += byteswritten;
+	writebytes -= byteswritten;
+      } else {
+	printf("\nWrite error in write_multifile():\n");
+	printf("\tMultifile is maximum length!\n\n");
+	return byteswritten / type;
+      }
+    } else {
+      printf("\nWrite error in write_multifile():\n");
+      printf("\tTried to write %d bytes, only wrote %d!\n\n", \
+	     number * type, byteswritten);
+      return byteswritten / type;
+    }
+  }
+  return byteswritten / type;
+}
+
+int fseek_multifile(multifile *mfile, long long offset, int whence)
+/* Set the multifile position indicator as in fseek().   */
+/*   'offset' is the file offset in bytes.               */
+/*   'whence' is either SEEK_SET, SEEK_CUR, or SEEK_END. */
+/*   Note:  Return is 0 for success, -1 for failure      */
+{
+  int findex=0;
+  long long cumlen=0;
+
+  if (whence==SEEK_SET){
+    mfile->position = offset;
+  } else if (whence==SEEK_CUR){
+    mfile->position += offset;
+  } else if (whence==SEEK_END){
+    mfile->position = mfile->length + offset;
+  } else {
+      printf("\nSeek error in seek_multifile():\n");
+      printf("\tValue for 'whence' was %d!\n\n", \
+	     whence);
+  }
+  if (mfile->position < 0) 
+    mfile->position = 0;
+  if (mfile->position > mfile->length) 
+    mfile->position = mfile->length;
+  while (cumlen < mfile->position){
+    cumlen += mfile->filelens[findex];
+    findex++;
+  }
+  findex--;
+  mfile->currentfile = findex;
+  mfile->currentpos = mfile->position - 
+    (cumlen - mfile->filelens[findex]);
+  return chkfseek(mfile->fileptrs[findex], 
+		  mfile->currentpos, SEEK_SET);
+}
+
+void rewind_multifile(multifile *mfile)
+/* Rewind a multifile. */
+{
+  fseek_multifile(mfile, 0, SEEK_SET);
+}
+
+long long ftell_multifile(multifile *mfile)
+/* Report the current position of a muiltifile. */
+{
+  return mfile->position;
+}
