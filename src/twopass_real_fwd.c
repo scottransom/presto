7a1ddd5c3fb79aa5e1c03dc0be2b4e38642ea6af
Scott Ransom <sransom@nrao.edu>
2000-10-06 21:09:18 +0000

    Many changes.  Almost works.  Still must fix the complex to real converison.
    git-svn-id: file://localhost/data1/git/presto_svn/trunk@382 5fac236c-a0d8-0310-9745-ef116914eda1

--- src/twopass_real_fwd.c
+++ src/twopass_real_fwd.c
@@ -9,34 +9,21 @@ extern long long find_blocksize(long long n1, long long n2);
 void realfft_scratch_fwd(multifile* infile, multifile* scratch, 
 			 long long nn)
 {
-  long long n1, n2, b, b2, s, s1, s2, j, k;
-  int i, i1, i2, i3, i4, move_size;
-  int m, ct, ind, lorowind, hirowind, n2by2;
+  long long n1, n2, bb, bb2, fp1, fp2, ii, jj, kk, ll, s1, s2;
+  int i1, i2, i3, i4, move_size, n2by2;
   unsigned char *move;
-  rawtype *data, *p1, *p2, *p3, *p4;
+  rawtype *data, *dp, *p1, *p2, *p3, *p4;
   float *f1, *datap;
-  double tmp = 0.0, h1r, h1i, h2r, h2i;
+  double tmp1, tmp2, h1r, h1i, h2r, h2i;
   double h2rwr, h2rwi, h2iwr, h2iwi;
   double wtemp, wpi, wpr, wi, wr, theta, delta;
 
   if (nn < 2)
     return;
 
-  /* treat the input data as a n1 x n2 matrix */
-  /* n2 >= n1 */
+  /* Treat the input data as a n1 (rows) x n2 (cols) */
+  /* matrix.  Make sure that n2 >= n1.               */
 
-   for (n1 = 1, n2 = 0; n1 < nn / 2; n1 <<= 1, n2++);
-   n1 = n2 >> 1;
-   n2 -= n1;
-   
-   n1 = 1 << n1;
-   n2 = 1 << n2;
-   b = Maxblocksize / n1;
-   if (b > n1)
-     b = n1;
-   printf("b = %lld, n1 = %lld, n2 = %lld, nn = %lld (%lld)\n", 
-	 b, n1, n2, nn, n1*n2);
-   
   if (nn % 4 != 0){
     printf("\nLength of FFT in twopassfft_real_fwd()\n");
     printf("   must be divisible by 4.\n\n");
@@ -49,73 +36,86 @@ void realfft_scratch_fwd(multifile* infile, multifile* scratch,
     exit(1);
   }
   n1 = nn / (2 * n2);
-  b = find_blocksize(n1, n2);
-  printf("b = %lld, n1 = %lld, n2 = %lld, nn = %lld (%lld)\n", 
-	 b, n1, n2, nn, n1*n2);
-  if (b==0 || b % 2){
+  bb = find_blocksize(n1, n2);
+  printf("bb = %lld, n1 = %lld, n2 = %lld, nn = %lld (%lld)\n", 
+	 bb, n1, n2, nn, n1*n2);
+  if (bb==0 || bb % 2){
     printf("\nCan't factor the FFT length in twopassfft_real_fwd()\n");
     printf("   into useful sizes.\n\n");
     exit(1);
   }
 
-  /* first do n2 transforms of length n1 */
-  /* by fetching n1 x b blocks in memory */
+  /* First do n2 transforms of length n1 by  */
+  /* fetching size bb x n1 blocks in memory. */
 
-  data = gen_rawvect(nn / 2 < b * n2 ? nn / 2 : b * n2);
-  datap = (float *) data;
+  data = gen_rawvect(bb * n2);
 
-  /* transpose scratch space */
+  /* Transpose scratch space */
 
-  move_size = (b + n1) / 2;
+  move_size = (bb + n1) / 2;
   move = (unsigned char *)malloc(move_size);
 
-  for (k = 0; k < n2; k += b) {
-    /* read the data from the input file in b x b blocks */
-    for (j = 0, p1 = data, s = k; j < n1; j += b, p1 += b) {
-      for (m = 0, p2 = p1; m < b; m++, p2 += n1, s += n2) {
-	fseek_multifile(infile, sizeof(rawtype) * s, SEEK_SET);
-	fread_multifile(p2, sizeof(rawtype), b, infile);
-      }
+  for (ii=0; ii<n2; ii+=bb){
 
-      /* transpose the b x b block */
+    /* Read a n1 (rows) x bb (cols) block of data */
 
-      transpose_fcomplex((fcomplex *) p1, b, n1, move, move_size); 
+    dp = data;
+    for (jj=0; jj<n1; jj++){
+      fp1 = sizeof(rawtype) * (jj * n2 + ii); /* File ptr */
+      fseek_multifile(infile, fp1, SEEK_SET);
+      fread_multifile(dp, sizeof(rawtype), bb, infile);
+      dp += bb; /* Data ptr */
     }
 
-    /* do b transforms of size n1 */
+    /* Transpose the n1 (rows) x bb (cols) block of data */
 
-    for (j = 0, p1 = data; j < b; j++, p1 += n1)
-      tablesixstepfft((fcomplex *) p1, n1, -1);
+    transpose_fcomplex(data, n1, bb, move, move_size); 
 
-    /* then multiply the matrix A_jk by exp(- 2 pi i j k / nn) */
-    /* Use recursion formulas from NR                          */
+    /* Do bb transforms of length n1 */
 
-    for (j = 0, f1 = (float *) data; j < b; j++, f1 += 2 * n1) {
-      theta = -(j + k) * TWOPI / (nn >> 1);
+    for (jj=0; jj<bb; jj++)
+      fftwcall(data + jj * n1, n1, -1);
+
+    /* Multiply the matrix A(ii,jj) by exp(- 2 pi i jj ii / nn). */
+    /* Use recursion formulas from Numerical Recipes.            */
+
+    f1 = (float *)data;
+    for (jj=0; jj<bb; jj++){
+      theta = -(jj + ii) * TWOPI / (nn >> 1);
       wtemp = sin(0.5 * theta);
       wpr = -2.0 * wtemp * wtemp;
       wpi = sin(theta);
       wr = 1.0 + wpr;
       wi = wpi;
-      for (ct = 1, ind = 2; ct < n1; ct++, ind += 2) {
-	f1[ind] = (tmp = f1[ind]) * wr - f1[ind + 1] * wi;
-	f1[ind + 1] = f1[ind + 1] * wr + tmp * wi;
-	wr = (wtemp = wr) * wpr - wi * wpi + wr;
+      i1 = 2;
+      i2 = 3;
+      for (kk=1; kk<n1; kk++){
+	tmp1 = f1[i1];
+	tmp2 = f1[i2];
+	f1[i1] = tmp1 * wr - tmp2 * wi;
+	f1[i2] = tmp2 * wr + tmp1 * wi;
+	wtemp = wr;
+	wr = wtemp * wpr - wi * wpi + wr;
 	wi = wi * wpr + wtemp * wpi + wi;
+	i1 += 2;
+	i2 += 2;
       }
+      f1 += 2 * n1;
     }
 
-    /* write the data to the scratch file */
-    fwrite_multifile(data, sizeof(rawtype), b * n1, scratch);
+    /* Write the data to the scratch file */
+    
+    fwrite_multifile(data, sizeof(rawtype), bb * n1, scratch);
+
   }
+
   free(move);
 
-  /* then do n1 transforms of length n2          */
-  /* by fetching 2 x (n2 x b) blocks in memory   */
-  /* and recombine blocks to make real transform */
+  /* Now do n1 transforms of length n2 by fetching      */
+  /* groups of 2 size n2 (rows) x bb2 (cols) blocks and */
+  /* then recombining blocks to make a real transform.  */
 
-  b2 = b;
-  b = b2 >> 1;
+  bb2 = bb >> 1;
 
   /* Some values for the trig recursion below: */
 
@@ -126,71 +126,75 @@ void realfft_scratch_fwd(multifile* infile, multifile* scratch,
 
   /* transpose scratch space */
 
-  move_size = (b + n2) / 2;
+  move_size = (bb + n2) / 2;
   move = (unsigned char *)malloc(move_size);
-  
-  for (k = 0; k < (n1 / 2); k += b) {
+  tmp1 = sizeof(rawtype) * n1;
 
-    /* read the data from the input file in b x b blocks */
+  for (ii=0; ii<(n1/2); ii+=bb2) {
 
-    for (j = 0, s1 = k + 1, s2 = n1 - k - b, p1 = data; \
-	 j < n2; j += b2, p1 += b2) {
+    /* Read two n2 (rows) x bb2 (cols) blocks from the file */
+    /* The first block comes from the start of the file and */
+    /* the second block comes from the end.                 */
 
-      for (m = 0, p2 = p1; m < b2; m++, p2 += n2) {
-	fseek_multifile(scratch, sizeof(rawtype) * s1, SEEK_SET);
-	fread_multifile(p2, sizeof(rawtype), b, scratch);
-	fseek_multifile(scratch, sizeof(rawtype) * s2, SEEK_SET);
-	fread_multifile(p2 + b, sizeof(rawtype), b, scratch);
-	s1 += n1;
-	s2 += n1;
-      }
-
-      /* transpose the b2 x b2 blocks */
-
-      transpose_fcomplex((fcomplex *) p1, b, n2, move, move_size); 
+    dp = data;
+    fp1 = sizeof(rawtype) * ii;              /* File ptr */
+    fp2 = sizeof(rawtype) * (n1 - ii - bb2); /* File ptr */
+    for (jj=0; jj<n2; jj++){
+      fseek_multifile(scratch, fp1, SEEK_SET);
+      fread_multifile(dp, sizeof(rawtype), bb2, scratch);
+      dp += bb2;   /* Data ptr */
+      fp1 += tmp1; /* File ptr */
+      fseek_multifile(scratch, fp2, SEEK_SET);
+      fread_multifile(dp, sizeof(rawtype), bb2, scratch);
+      dp += bb2;   /* Data ptr */
+      fp2 += tmp1; /* File ptr */
     }
 
-    /* do 2*b transforms of size n2y */
+    /* Transpose the n2 (rows) x bb (cols) block of data */
 
-    for (j = 0, p1 = data; j < b2; j++, p1 += n2)
-      tablesixstepfft((fcomplex *) p1, n2, -1);
+    transpose_fcomplex(data, n2, bb, move, move_size); 
 
-    /* transpose the b2 x b2 blocks */
+    /* Do bb transforms of length n2 */
 
-    for (j = 0, p1 = data; j < n2; j += b2, p1 += b2)
-      transpose_fcomplex((fcomplex *) p1, b, n2, move, move_size); 
+    for (jj=0; jj<bb; jj++)
+      fftwcall(data + jj * n2, n2, -1);
+
+    /* Transpose the bb (rows) x n2 (cols) block of data */
+
+    transpose_fcomplex(data, bb, n2, move, move_size); 
 
     /* File pointers: */
-    s1 = k + 1;
-    s2 = nn / 2 - k - b;
+
+    s1 = ii + 1;
+    s2 = nn / 2 - ii - bb2;
 
     /* Data pointers: */
-    p1 = data;
-    p3 = data + n2 * b2 - b;
 
-    for (j = 0; j < n2; j += b2, p1 += b2, p3 -= b2) {
+    datap = (float *) data;
+    p1 = data;
+    p3 = data + n2 * bb - bb2;
+
+    for (jj=0; jj<n2; jj+=bb, p1+=bb, p3-=bb){
 
       p2 = p1;
       p4 = p3;
 
-      for (m = 0; m < b2; m++, p2 += n2, p4 -= n2) {
+      for (kk=0; kk<bb; kk++, p2+=n2, p4-=n2){
 
 	/* Start the trig recursion: */
 
-	theta = ((j + m) * n1 + k + 1) * delta;
+	theta = ((jj + kk) * n1 + ii + 1) * delta;
 	wr = cos(theta);
 	wi = sin(theta);
 
 	/* Combine n and N/2-n terms as per Numerical Recipes. */
 
-	lorowind = 2 * (m * n2 + j);
-	hirowind = 2 * (n2 * (b2 - m) - j - b);
-	i1 = lorowind;
+	i1 = 2 * (kk * n2 + jj);
 	i2 = i1 + 1;
-	i3 = hirowind + b2 - 2;
+	i3 = 2 * (n2 * (bb - kk) - jj - bb2) + bb - 2;
 	i4 = i3 + 1;
 
-	for (i = 0; i < b; i++) {
+	for (ll=0; ll<bb2; ll++){
 	  h1r = 0.5 * (datap[i1] + datap[i3]);
 	  h1i = 0.5 * (datap[i2] - datap[i4]);
 	  h2r = 0.5 * (datap[i2] + datap[i4]);
@@ -203,7 +207,8 @@ void realfft_scratch_fwd(multifile* infile, multifile* scratch,
 	  datap[i2] = h1i + h2iwr + h2rwi;
 	  datap[i3] = h1r - h2rwr + h2iwi;
 	  datap[i4] = -h1i + h2iwr + h2rwi;
-	  wr = (wtemp = wr) * wpr - wi * wpi + wr;
+	  wtemp = wr;
+	  wr = wtemp * wpr - wi * wpi + wr;
 	  wi = wi * wpr + wtemp * wpi + wi;
 	  i1 += 2;
 	  i2 += 2;
@@ -213,11 +218,11 @@ void realfft_scratch_fwd(multifile* infile, multifile* scratch,
 
 	/* Write the "low" freqs: */
 	fseek_multifile(infile, sizeof(rawtype) * s1, SEEK_SET);
-	fwrite_multifile(p2, sizeof(rawtype), b, infile);
+	fwrite_multifile(p2, sizeof(rawtype), bb2, infile);
 
 	/* Write the "high" freqs: */
 	fseek_multifile(infile, sizeof(rawtype) * s2, SEEK_SET);
-	fwrite_multifile(p4, sizeof(rawtype), b, infile);
+	fwrite_multifile(p4, sizeof(rawtype), bb2, infile);
 
 	s1 += n1;
 	s2 -= n1;
@@ -234,7 +239,7 @@ void realfft_scratch_fwd(multifile* infile, multifile* scratch,
 
   /* Read the n2 data points */
 
-  for (j = 0, s1 = 0, f1 = datap; j < n2; j++, s1 += n1, f1 += 2) {
+  for (jj=0, s1=0, f1=datap; jj<n2; jj++, s1+=n1, f1+=2){
     fseek_multifile(scratch, sizeof(rawtype) * s1, SEEK_SET);
     fread_multifile(f1, sizeof(float), 2, scratch);
   }
@@ -245,8 +250,9 @@ void realfft_scratch_fwd(multifile* infile, multifile* scratch,
 
   /* Do the special cases of freq=0 and Nyquist freq */
 
-  datap[0] = (tmp = datap[0]) + datap[1];
-  datap[1] = tmp - datap[1];
+  tmp1 = datap[0];
+  datap[0] = tmp1 + datap[1];
+  datap[1] = tmp1 - datap[1];
 
   /* Some values for the trig recursion below: */
 
@@ -263,7 +269,7 @@ void realfft_scratch_fwd(multifile* infile, multifile* scratch,
   i3 = n2 * 2 - 2;
   i4 = i3 + 1;
 
-  for (j = 1; j < n2by2; j++) {
+  for (jj = 1; jj < n2by2; jj++){
     h1r = 0.5 * (datap[i1] + datap[i3]);
     h1i = 0.5 * (datap[i2] - datap[i4]);
     h2r = 0.5 * (datap[i2] + datap[i4]);
@@ -286,7 +292,7 @@ void realfft_scratch_fwd(multifile* infile, multifile* scratch,
 
   /* Write the n2 data points and clean up */
 
-  for (j = 0, s1 = 0, f1 = datap; j < n2; j++, s1 += n1, f1 += 2) {
+  for (jj=0, s1=0, f1=datap; jj<n2; jj++, s1+=n1, f1+=2){
     fseek_multifile(infile, sizeof(rawtype) * s1, SEEK_SET);
     fwrite_multifile(f1, sizeof(float), 2, infile);
   }
