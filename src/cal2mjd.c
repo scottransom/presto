2148c0419f209597db7cb8cc6f576f42625336ac
Scott Ransom <sransom@nrao.edu>
2001-04-07 04:29:05 +0000

    Added calendar to MJD function (in cldj.c) from SLALIB Added command line cal2mjd program (like mjd2cal).
    git-svn-id: file://localhost/data1/git/presto_svn/trunk@542 5fac236c-a0d8-0310-9745-ef116914eda1

---
+++ src/cal2mjd.c
@@ -0,0 +1,78 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+double slaCldj(int iy, int im, int id, int *j);
+
+/* Does not include jump discontinuities from leap seconds.  */
+/* I don't really know if it should !                        */
+
+#ifdef USEDMALLOC
+#include "dmalloc.h"
+#endif
+
+int main(int argc, char *argv[])
+{
+  int year, month=1, day=1, hour=0, min=0, err;
+  double MJD, fracday, sec=0.0;
+  
+  if (argc < 2) {
+    printf("\nUsage:  'cal2mjd YYYY MM DD HH MM SS.SSSSS'\n\n");
+    exit(1);
+  }
+  year = strtol(argv[1], NULL, 10);
+  if (argc > 2){
+    month = strtol(argv[2], NULL, 10);
+    if (month < 1 || month > 12){
+      printf("\nmonth = %d is out-of-range.\n", month);
+      exit(1);
+    }
+  }
+  if (argc > 3){
+    day = strtol(argv[3], NULL, 10);
+    if (day < 1 || day > 31){
+      printf("\nday = %d is out-of-range.\n", month);
+      exit(1);
+    }
+  }
+  if (argc > 4){
+    hour = strtol(argv[4], NULL, 10);
+    if (hour == 24)
+      hour = 0;
+    if (hour < 0 || hour > 23){
+      printf("\nhour = %d is out-of-range.\n", month);
+      exit(1);
+    }
+  }
+  if (argc > 5){
+    min = strtol(argv[5], NULL, 10);
+    if (min < 0 || min > 59){
+      printf("\nmin = %d is out-of-range.\n", month);
+      exit(1);
+    }
+  }
+  if (argc > 6){
+    sec = strtod(argv[6], NULL);
+    if (sec < 0.0 || sec >= 60.0){
+      printf("\nsec = %d is out-of-range.\n", month);
+      exit(1);
+    }
+  }
+  fracday = (hour + (min + (sec / 60.0)) / 60.0) / 24.0;
+  MJD = slaCldj(year, month, day, &err);
+  MJD += fracday;
+  if (err == 1) {
+    printf("\nTry again.  Bad year.\n\n");
+    exit(1);
+  }
+  if (err == 2) {
+    printf("\nTry again.  Bad month.\n\n");
+    exit(1);
+  }
+  if (err == 3) {
+    printf("\nTry again.  Bad day.\n\n");
+    exit(1);
+  }
+  printf("\nMJD is %17.11f\n\n", MJD);
+  exit(0);
+}
---
+++ src/cldj.c
@@ -0,0 +1,68 @@
+/*
+ *     - - - - - 
+ *      C L D J 
+ *     - - - - - 
+ *
+ *  Gregorian Calendar to Modified Julian Date
+ *
+ *  Given: 
+ *     IY,IM,ID     int    year, month, day in Gregorian calendar 
+ *
+ *  Returned: 
+ *     DJM          dp     modified Julian Date (JD-2400000.5) for 0 hrs 
+ *     J            int    status: 
+ *                           0 = OK 
+ *                           1 = bad year   (MJD not computed) 
+ *                           2 = bad month  (MJD not computed) 
+ *                           3 = bad day    (MJD computed) 
+ *
+ *  The year must be -4699 (i.e. 4700BC) or later. 
+ *
+ *  The algorithm is derived from that of Hatcher 1984 
+ *  (QJRAS 25, 53-55). 
+ *
+ *  P.T.Wallace   Starlink   11 March 1998 
+ *
+ *  Copyright (C) 1998 Rutherford Appleton Laboratory 
+ *
+ */
+
+static int mtab[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
+
+double slaCldj(int iy, int im, int id, int *j)
+{
+  double mjd=0.0;
+  /*  Month lengths in days */
+
+  /* Preset status */
+  *j = 0;
+  /* Validate year */
+  if (iy < -4699) {
+    *j = 1;
+  } else {
+    /* Validate month */
+    if (im >= 1 && im <= 12) {
+      /* Allow for leap year */
+      if (iy % 4 == 0) {
+	mtab[1] = 29;
+      } else {
+	mtab[1] = 28;
+      }
+      if (iy % 100 == 0 && iy % 400 != 0) {
+	mtab[1] = 28;
+      }
+      /* Validate day */
+      if (id < 1 || id > mtab[im-1]) {
+	*j = 3;
+      }
+      /* Modified Julian Date */
+      mjd = (double) ((iy - (12 - im) / 10 + 4712) * 1461 / 4 + \
+		      ((im + 9) % 12 * 306 + 5) / 10 - \
+		      (iy - (12 - im) / 10 + 4900) / 100 * \
+		      3 / 4 + id - 2399904);
+    } else { /* Bad month */
+      *j = 2;
+    }
+  }
+  return mjd;
+}
