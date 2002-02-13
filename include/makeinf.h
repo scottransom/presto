99674c7e9b2d151dc97d8c1f762fd2a237ec4e1a
Scott Ransom <sransom@nrao.edu>
2002-02-13 21:22:32 +0000

    Re-ordered the infodata struct to make it more friendly to MPI.
    git-svn-id: file://localhost/data1/git/presto_svn/trunk@648 5fac236c-a0d8-0310-9745-ef116914eda1

--- include/makeinf.h
+++ include/makeinf.h
@@ -19,38 +19,38 @@
 #define MAXNUMONOFF 20
 
 typedef struct INFODATA {
-    char name[200];		/* Data file name without suffix         */
-    char object[100];		/* Object being observed                 */
-    int ra_h;			/* Right ascension hours (J2000)         */
-    int ra_m;			/* Right ascension minutes (J2000)       */
     double ra_s;		/* Right ascension seconds (J2000)       */
-    int dec_d;			/* Declination degrees (J2000)           */
-    int dec_m;			/* Declination minutes (J2000)           */
-    double dec_s;		/* Declination seconds (J2000)           */
-    char telescope[40];		/* Telescope used                        */
-    char instrument[100];	/* Instrument used                       */
-    char observer[100];		/* Observer[s] for the data set          */
+    double dec_s;		/* Declination seconds (J2000)           */ 
     double N;		        /* Number of bins in the time series     */
     double dt;	 	        /* Width of each time series bin (sec)   */
-    int numonoff;		/* The number of onoff pairs in the data */
-    double onoff[MAXNUMONOFF*2];/* Bin number pairs where obs is "on"    */
     double fov;			/* Diameter of Beam or FOV in arcsec     */
-    int mjd_i;			/* Epoch of observation (MJD) int part   */
     double mjd_f;		/* Epoch of observation (MJD) frac part  */
-    int bary;			/* Barycentered?  1=yes, 0=no            */
-    char band[40];		/* Type of observation (EM band)         */
     double dm;			/* Radio -- Dispersion Measure (cm-3 pc) */
     double freq;		/* Radio -- Low chan central freq (Mhz)  */
     double freqband;		/* Radio -- Total Bandwidth (Mhz)        */
-    int num_chan;		/* Radio -- Number Channels              */
     double chan_wid;		/* Radio -- Channel Bandwidth (Mhz)      */
-    char filt[7];		/* IR,Opt,UV -- Photometric Filter       */
     double wavelen;		/* IR,Opt,UV -- central wavelength (nm)  */
     double waveband;		/* IR,Opt,UV -- bandpass (nm)            */
     double energy;		/* x-ray,gamma -- central energy (kev)   */
     double energyband;		/* x-ray,gamma -- energy bandpass (kev)  */
-    char analyzer[100];		/* Who analyzed the data                 */
+    double onoff[MAXNUMONOFF*2];/* Bin number pairs where obs is "on"    */
+    int num_chan;		/* Radio -- Number Channels              */
+    int mjd_i;			/* Epoch of observation (MJD) int part   */
+    int ra_h;			/* Right ascension hours (J2000)         */
+    int ra_m;			/* Right ascension minutes (J2000)       */
+    int dec_d;			/* Declination degrees (J2000)           */
+    int dec_m;			/* Declination minutes (J2000)           */  
+    int bary;			/* Barycentered?  1=yes, 0=no            */
+    int numonoff;		/* The number of onoff pairs in the data */ 
     char notes[500];		/* Any additional notes                  */
+    char name[200];		/* Data file name without suffix         */
+    char object[100];		/* Object being observed                 */ 
+    char instrument[100];	/* Instrument used                       */
+    char observer[100];		/* Observer[s] for the data set          */
+    char analyzer[100];		/* Who analyzed the data                 */
+    char telescope[40];		/* Telescope used                        */
+    char band[40];		/* Type of observation (EM band)         */
+    char filt[7];		/* IR,Opt,UV -- Photometric Filter       */
 } infodata;
 
 void readinf(infodata * data, char *filenm);
