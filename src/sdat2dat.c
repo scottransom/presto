215e754872c9fee299eff20fb0a54d96d27151be
Scott Ransom <sransom@nrao.edu>
2002-09-14 03:16:52 +0000

    Fixed a couple small bugs.  Also a little cleanup.
    git-svn-id: file://localhost/data1/git/presto_svn/trunk@734 5fac236c-a0d8-0310-9745-ef116914eda1

--- src/sdat2dat.c
+++ src/sdat2dat.c
@@ -14,13 +14,13 @@
 int main(int argc, char *argv[])
 {
   FILE *infile, *outfile;
-  int ii, offset, numread;
+  int ii, numread;
   long long N=0;
   float offset=0.0, fbuffer[sizeof(float)*BUFFLEN];
   short sbuffer[sizeof(short)*BUFFLEN];
   char *outname;
 
-  if (argc != 2 || argc != 3) {
+  if (argc != 2 && argc != 3){
     printf("\nUsage:  sdat2dat sdatfilename [offset to add]\n\n");
     exit(1);
   }
@@ -40,12 +40,13 @@ int main(int argc, char *argv[])
 
   if (argc==3){
     offset = strtod(argv[2], NULL);
-    printf(" Converting, adding %d, and writing to '%s'...", offset, outname);
+    printf(" Converting, adding %g, and writing to '%s'...", offset, outname);
   } else {
     printf(" Converting the data and writing to '%s'...", outname);
   }
   fflush(NULL);
 
+  infile = chkfopen(argv[1], "rb");
   outfile = chkfopen(outname, "wb");
   while ((numread=chkfread(sbuffer, sizeof(short), BUFFLEN, infile))){
     N += numread;
@@ -53,7 +54,7 @@ int main(int argc, char *argv[])
       fbuffer[ii] = (float)(sbuffer[ii])+offset;
     fwrite(fbuffer, sizeof(float), numread, outfile);
   }
-  printf("done.\n Wrote %d points.", N);
+  printf("done.\n Wrote %lld points.\n\n", N);
   fclose(infile);
   fclose(outfile);
   free(outname);
