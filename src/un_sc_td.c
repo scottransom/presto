464e610c2787021b9641d106dc8e8c49b6068da0
Scott Ransom <sransom@nrao.edu>
2002-12-04 04:02:38 +0000

    Added to CVS.
    git-svn-id: file://localhost/data1/git/presto_svn/trunk@796 5fac236c-a0d8-0310-9745-ef116914eda1

---
+++ src/un_sc_td.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define HDRLEN 640
+#define DATLEN 49152
+
+int main(int argc, char* argv[]){
+  FILE *datfile, *rawfile;
+  char hdr[HDRLEN], filenm[100];
+  unsigned char dat[DATLEN];
+  int ii=0, numread;
+
+  if (argc!=2){
+    printf("usage:  un_sc_td basefilename\n");
+    exit(0);
+  }
+
+  sprintf(filenm, "%s.hdr", argv[1]);
+  datfile = fopen(filenm, "r");
+  if ((numread=fread(hdr, sizeof(char), HDRLEN+8, datfile))!=HDRLEN){
+    printf("  Problem reading the header file '%s'\n", argv[1]);
+  }
+  fclose(datfile);
+
+  sprintf(filenm, "%s.dat", argv[1]);
+  datfile = fopen(filenm, "r");
+  sprintf(filenm, "%s.pkmb", argv[1]);
+  rawfile = fopen(filenm, "w");
+  while (fread(dat, sizeof(unsigned char), DATLEN+8, datfile)){
+    fwrite(hdr+4, sizeof(char), HDRLEN, rawfile);
+    fwrite(dat+4, sizeof(unsigned char), DATLEN, rawfile);
+    ii++;
+  }
+  fclose(datfile);
+  fclose(rawfile);
+  printf("Done.  Wrote %d blocks.\n", ii);
+  return 0;
+}
