c2106aaf0ba46b99705cb8ae097b071209368134
Scott Ransom <sransom@nrao.edu>
2003-03-31 21:29:41 +0000

    Added to CVS.
    git-svn-id: file://localhost/data1/git/presto_svn/trunk@842 5fac236c-a0d8-0310-9745-ef116914eda1

---
+++ include/gmrt.h
@@ -0,0 +1,24 @@
+void set_GMRT_static(int numchan, double dt);
+void get_GMRT_file_info(FILE *files[], char *datfilenms[], int numfiles, 
+			long long *N, int *ptsperblock, int *numchan, double *dt, 
+			double *T, int output);
+void GMRT_update_infodata(int numfiles, infodata *idata);
+int skip_to_GMRT_rec(FILE *infiles[], int numfiles, int rec);
+int read_GMRT_rawblock(FILE *infiles[], int numfiles, unsigned char *data, 
+		       int *padding);
+int read_GMRT_rawblocks(FILE *infiles[], int numfiles, unsigned char rawdata[], 
+			int numblocks, int *padding);
+int read_GMRT(FILE *infiles[], int numfiles, float *data, int numpts, 
+	      double *dispdelays, int *padding, int *maskchans, 
+	      int *nummasked, mask *obsmask);
+void get_GMRT_channel(int channum, float chandat[], 
+		      unsigned char rawdata[], int numblocks);
+int prep_GMRT_subbands(unsigned char *rawdata, float *data, double *dispdelays, 
+		       int numsubbands, int transpose, int *maskchans, 
+		       int *nummasked, mask *obsmask);
+int read_GMRT_subbands(FILE *infiles[], int numfiles, float *data, 
+		       double *dispdelays, int numsubbands, int transpose, 
+		       int *padding, int *maskchans, int *nummasked, 
+		       mask *obsmask);
+void GMRT_hdr_to_inf(char *datfilenm, infodata *idata);
+void convert_GMRT_block(short *indata, unsigned char *outdata);
