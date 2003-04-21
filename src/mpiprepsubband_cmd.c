33647b302843ac60ab3a0163df0424e23f29219b
Scott Ransom <sransom@nrao.edu>
2003-04-21 20:39:09 +0000

    Re-generated CLIG files after modifying the .cli file for mpiprepsubband.
    git-svn-id: file://localhost/data1/git/presto_svn/trunk@868 5fac236c-a0d8-0310-9745-ef116914eda1

--- src/mpiprepsubband_cmd.c
+++ src/mpiprepsubband_cmd.c
@@ -38,14 +38,16 @@ static Cmdline cmd = {
   /* ifsC = */ 0,
   /***** -wapp: Raw data in Wideband Arecibo Pulsar Processor (WAPP) format */
   /* wappP = */ 0,
-  /***** -clip: For WAPP format only:  Time-domain sigma to use for clipping.  If zero, no clipping is performed. */
-  /* clipP = */ 1,
-  /* clip = */ 0.0,
-  /* clipC = */ 1,
   /***** -numwapps: Number of WAPPs used with contiguous frequencies */
   /* numwappsP = */ 1,
   /* numwapps = */ 1,
   /* numwappsC = */ 1,
+  /***** -clip: Time-domain sigma to use for clipping (0.0 = no clipping, 6.0 = default */
+  /* clipP = */ 1,
+  /* clip = */ 6.0,
+  /* clipC = */ 1,
+  /***** -noclip: Do not clip the data.  (The default is to _always_ clip!) */
+  /* noclipP = */ 0,
   /***** -numout: Output this many values.  If there are not enough values in the original data file, will pad the output file with the average value */
   /* numoutP = */ 0,
   /* numout = */ (int)0,
@@ -841,7 +843,19 @@ showOptionValues(void)
     printf("-wapp found:\n");
   }
 
-  /***** -clip: For WAPP format only:  Time-domain sigma to use for clipping.  If zero, no clipping is performed. */
+  /***** -numwapps: Number of WAPPs used with contiguous frequencies */
+  if( !cmd.numwappsP ) {
+    printf("-numwapps not found.\n");
+  } else {
+    printf("-numwapps found:\n");
+    if( !cmd.numwappsC ) {
+      printf("  no values\n");
+    } else {
+      printf("  value = `%d'\n", cmd.numwapps);
+    }
+  }
+
+  /***** -clip: Time-domain sigma to use for clipping (0.0 = no clipping, 6.0 = default */
   if( !cmd.clipP ) {
     printf("-clip not found.\n");
   } else {
@@ -853,16 +867,11 @@ showOptionValues(void)
     }
   }
 
-  /***** -numwapps: Number of WAPPs used with contiguous frequencies */
-  if( !cmd.numwappsP ) {
-    printf("-numwapps not found.\n");
+  /***** -noclip: Do not clip the data.  (The default is to _always_ clip!) */
+  if( !cmd.noclipP ) {
+    printf("-noclip not found.\n");
   } else {
-    printf("-numwapps found:\n");
-    if( !cmd.numwappsC ) {
-      printf("  no values\n");
-    } else {
-      printf("  value = `%d'\n", cmd.numwapps);
-    }
+    printf("-noclip found:\n");
   }
 
   /***** -numout: Output this many values.  If there are not enough values in the original data file, will pad the output file with the average value */
@@ -978,7 +987,7 @@ void
 usage(void)
 {
   fprintf(stderr, "usage: %s%s", Program, "\
- -o outfile [-pkmb] [-gmrt] [-bcpm] [-if ifs] [-wapp] [-clip clip] [-numwapps numwapps] [-numout numout] [-nobary] [-DE405] [-lodm lodm] [-dmstep dmstep] [-numdms numdms] [-numsub numsub] [-downsamp downsamp] [-mask maskfile] [--] infile ...\n\
+ -o outfile [-pkmb] [-gmrt] [-bcpm] [-if ifs] [-wapp] [-numwapps numwapps] [-clip clip] [-noclip] [-numout numout] [-nobary] [-DE405] [-lodm lodm] [-dmstep dmstep] [-numdms numdms] [-numsub numsub] [-downsamp downsamp] [-mask maskfile] [--] infile ...\n\
     Converts a raw radio data file into many de-dispersed time-series (including barycentering).\n\
          -o: Root of the output file names\n\
              1 char* value\n\
@@ -988,12 +997,13 @@ usage(void)
         -if: For BPP format only:  A specific IF to use.\n\
              1 int value between 0 and 1\n\
       -wapp: Raw data in Wideband Arecibo Pulsar Processor (WAPP) format\n\
-      -clip: For WAPP format only:  Time-domain sigma to use for clipping.  If zero, no clipping is performed.\n\
-             1 float value between 0 and 20.0\n\
-             default: `0.0'\n\
   -numwapps: Number of WAPPs used with contiguous frequencies\n\
              1 int value between 1 and 7\n\
              default: `1'\n\
+      -clip: Time-domain sigma to use for clipping (0.0 = no clipping, 6.0 = default\n\
+             1 float value between 0 and 20.0\n\
+             default: `6.0'\n\
+    -noclip: Do not clip the data.  (The default is to _always_ clip!)\n\
     -numout: Output this many values.  If there are not enough values in the original data file, will pad the output file with the average value\n\
              1 int value between 1 and oo\n\
     -nobary: Do not barycenter the data\n\
@@ -1017,7 +1027,7 @@ usage(void)
              1 char* value\n\
      infile: Input data file name.  If the data is not in PKMB or EBPP format, it should be a single channel of single-precision floating point data.  In this case a '.inf' file with the same root filename must also exist (Note that this means that the input data file must have a suffix that starts with a period)\n\
              1...100 values\n\
-version: 31Mar03\n\
+version: 21Apr03\n\
 ");
   exit(EXIT_FAILURE);
 }
@@ -1074,6 +1084,16 @@ parseCmdline(int argc, char **argv)
       continue;
     }
 
+    if( 0==strcmp("-numwapps", argv[i]) ) {
+      int keep = i;
+      cmd.numwappsP = 1;
+      i = getIntOpt(argc, argv, i, &cmd.numwapps, 1);
+      cmd.numwappsC = i-keep;
+      checkIntLower("-numwapps", &cmd.numwapps, cmd.numwappsC, 7);
+      checkIntHigher("-numwapps", &cmd.numwapps, cmd.numwappsC, 1);
+      continue;
+    }
+
     if( 0==strcmp("-clip", argv[i]) ) {
       int keep = i;
       cmd.clipP = 1;
@@ -1084,13 +1104,8 @@ parseCmdline(int argc, char **argv)
       continue;
     }
 
-    if( 0==strcmp("-numwapps", argv[i]) ) {
-      int keep = i;
-      cmd.numwappsP = 1;
-      i = getIntOpt(argc, argv, i, &cmd.numwapps, 1);
-      cmd.numwappsC = i-keep;
-      checkIntLower("-numwapps", &cmd.numwapps, cmd.numwappsC, 7);
-      checkIntHigher("-numwapps", &cmd.numwapps, cmd.numwappsC, 1);
+    if( 0==strcmp("-noclip", argv[i]) ) {
+      cmd.noclipP = 1;
       continue;
     }
 
