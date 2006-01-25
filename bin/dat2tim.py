36ee7d2289a3e5d7d24806f6740a9554e05bf952
Scott Ransom <sransom@nrao.edu>
2006-01-25 03:14:08 +0000

    Bug fixes.  This now works.
    git-svn-id: file://localhost/data1/git/presto_svn/trunk@1130 5fac236c-a0d8-0310-9745-ef116914eda1

--- bin/dat2tim.py
+++ bin/dat2tim.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import scipy.io, struct, presto, sys
+import os, struct, presto, sys
 
 telescope_ids = {"Fake": 0, "Arecibo": 1, "Ooty": 2, "Nancay": 3,
                  "Parkes": 4, "Jodrell": 5, "GBT": 6, "GMRT": 7,
@@ -21,16 +21,21 @@ def infodata_to_sigproc_header(inf):
     hdr = prep_string("HEADER_START")
     hdr += prep_int("telescope_id", telescope_ids[inf.telescope.strip()])
     hdr += prep_int("machine_id", machine_ids[inf.instrument.strip()])
-    hdr += prep_int("data_type", 2)
+    hdr += prep_int("data_type", 2) # 1 = filterbank, 2 = timeseries
     hdr += prep_string("source_name")
     hdr += prep_string(inf.object)
     hdr += prep_int("barycentric", 0)
     hdr += prep_int("pulsarcentric", 0)
-    hdr += prep_double("src_raj", inf.ra_h*1000.0 + inf.ra_m*100.0 + inf.ra_s)
-    hdr += prep_double("src_dej", inf.dec_d*1000.0 + inf.dec_m*100.0 + inf.dec_s)
+    hdr += prep_double("src_raj", inf.ra_h*10000.0 + inf.ra_m*100.0 + inf.ra_s)
+    if inf.dec_d > 0.0:
+	hdr += prep_double("src_dej", inf.dec_d*10000.0 + inf.dec_m*100.0 + inf.dec_s)
+    else:
+	hdr += prep_double("src_dej", inf.dec_d*10000.0 - inf.dec_m*100.0 - inf.dec_s)
+    hdr += prep_int("nbits", 32)
     hdr += prep_int("nifs", 1)
+    hdr += prep_int("nchans", 1)
     hdr += prep_double("fch1", inf.freq)
-    hdr += prep_int("nbits", 32) # ??
+    hdr += prep_double("foff", 1)
     hdr += prep_double("tstart", inf.mjd_i + inf.mjd_f)
     hdr += prep_double("tsamp", inf.dt)
     hdr += prep_double("refdm", inf.dm)
@@ -46,5 +51,5 @@ if __name__ == "__main__":
     outfile = open(basefilenm+".tim", "wb")
     outfile.write(infodata_to_sigproc_header(inf))
     outfile.close()
-    # Do this the simple way
-    #os.sys("cat %s >> %s"%())
+    # Do this the simple (but kludgey) way
+    os.system("cat %s >> %s"%(sys.argv[1], basefilenm+".tim"))
