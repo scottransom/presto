52a3bdce0bf0967d4c922a8f84cf08aca1a61a91
Scott Ransom <sransom@nrao.edu>
2007-01-10 01:34:51 +0000

    Fixed (maybe) the 32bit version.
    git-svn-id: file://localhost/data1/git/presto_svn/trunk@1210 5fac236c-a0d8-0310-9745-ef116914eda1

--- lib/python/residuals.py
+++ lib/python/residuals.py
@@ -24,7 +24,7 @@ def read_residuals():
     r = residuals()
     rf = mIO.binary_file("resid2.tmp")
     # Fortran format: 2 longs (Fortran crap) + 9 doubles = 80 bytes
-    r.numTOAs = rf.size()/88
+    r.numTOAs = rf.size()/80
     r.bary_TOA = Numeric.zeros(r.numTOAs, 'd')
     r.postfit_phs = Numeric.zeros(r.numTOAs, 'd')
     r.postfit_sec = Numeric.zeros(r.numTOAs, 'd')
@@ -34,7 +34,7 @@ def read_residuals():
     r.uncertainty = Numeric.zeros(r.numTOAs, 'd')
     r.prefit_phs = Numeric.zeros(r.numTOAs, 'd')
     for ii in range(r.numTOAs):
-        struct = rf.fort_read("idddddddddi")
+        struct = rf.fort_read("ddddddddd")
 	(r.bary_TOA[ii], 
 	 r.postfit_phs[ii],
 	 r.postfit_sec[ii], 
@@ -42,8 +42,8 @@ def read_residuals():
 	 r.bary_freq[ii], 
 	 r.weight[ii], 
 	 r.uncertainty[ii], 
-	 r.prefit_phs[ii]) = (struct[1], struct[2], struct[3], struct[4], \
-	                      struct[5], struct[6], struct[7], struct[8])
+	 r.prefit_phs[ii]) = (struct[0], struct[1], struct[2], struct[3], \
+	                      struct[4], struct[5], struct[6], struct[7])
     rf.close()
     if not Numeric.nonzero(r.orbit_phs): del r.orbit_phs
     if not Numeric.nonzero(r.bary_freq): del r.bary_freq
@@ -57,7 +57,7 @@ def read_residuals_64bit():
     rf = open("resid2.tmp")
     # Fortran format: 2 longs (Fortran crap) + 9 doubles = 80 bytes
     rf.seek(0, 2)
-    r.numTOAs = rf.tell()/88
+    r.numTOAs = rf.tell()/80
     rf.seek(0, 0)
     r.bary_TOA = Numeric.zeros(r.numTOAs, 'd')
     r.postfit_phs = Numeric.zeros(r.numTOAs, 'd')
@@ -68,7 +68,7 @@ def read_residuals_64bit():
     r.uncertainty = Numeric.zeros(r.numTOAs, 'd')
     r.prefit_phs = Numeric.zeros(r.numTOAs, 'd')
     for ii in range(r.numTOAs):
-	block = rf.read(88)
+	block = rf.read(80)
 	struct = Numeric.fromstring(block, 'd')
 	(r.bary_TOA[ii], 
 	 r.postfit_phs[ii],
