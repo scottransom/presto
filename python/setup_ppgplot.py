4a62891f8d0c7f63185a69526092818b2d092a8f
Scott Ransom <sransom@nrao.edu>
2007-02-14 14:34:31 +0000

    Cleanups and also added a ppgplot specific setup file.
    git-svn-id: file://localhost/data1/git/presto_svn/trunk@1215 5fac236c-a0d8-0310-9745-ef116914eda1

--- python/setup_ppgplot.py
+++ python/setup_ppgplot.py
@@ -16,9 +16,6 @@ include_dirs = []
 ppgplot_libraries = ["cpgplot", "pgplot", "X11", "png", "m", "g2c"]
 ppgplot_library_dirs = ["/usr/X11R6/lib"]
 
-presto_libraries = ["presto", "fftw3f", "m"]
-presto_library_dirs = []
-
 # Use NumPy instead of Numeric or numarray
 make_extension = Extension
 #include_dirs.append(numpy.get_numpy_include())
@@ -33,11 +30,6 @@ if os.name == "posix":
         ppgplot_include_dirs.append(os.environ["PGPLOT_DIR"])
     else:
         print >>sys.stderr, "PGPLOT_DIR env var not defined!"
-    if os.environ.has_key("PRESTO"):
-        presto_library_dirs.append(os.path.join(os.environ["PRESTO"], "lib"))
-        presto_include_dirs.append(os.path.join(os.environ["PRESTO"], "include"))
-    else:
-        print >>sys.stderr, "PRESTO env var not defined!"
 else:
     raise Exception, "os not supported"
 
@@ -49,23 +41,16 @@ ext_ppgplot = make_extension('_ppgplot',
                              define_macros=define_macros,
                              extra_compile_args=extra_compile_args)
 
-ext_presto = make_extension('_presto',
-                            [os.path.join('presto_src', 'presto_wrap.c')],
-                            include_dirs=presto_include_dirs,
-                            libraries=presto_libraries,
-                            library_dirs=presto_library_dirs,
-                            define_macros=define_macros,
-                            extra_compile_args=extra_compile_args)
 
 ###################################################################
 # the package
 #
 
-setup(name="PRESTO",
+setup(name="ppgplot",
       version="0.99",
-      description="Python interfaces to PGPLOT and PRESTO",
+      description="Python interface to PGPLOT",
       author="Scott Ransom (ppgplot from Nick Patavlis)",
       author_email="sransom@nrao.edu",
-      packages=['ppgplot', "presto"],
-      package_dir={'ppgplot':'ppgplot_src','presto':'presto_src'},
-      ext_modules=[ext_ppgplot,ext_presto])
+      packages=['ppgplot'],
+      package_dir={'ppgplot':'ppgplot_src'},
+      ext_modules=[ext_ppgplot])
