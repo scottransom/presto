4ee60a58ee800bcf6fd5796b1aa8620df0c65273
Scott Ransom <sransom@nrao.edu>
2007-08-03 21:07:19 +0000

    Updated to modern numpy.
    git-svn-id: file://localhost/data1/git/presto_svn/trunk@1270 5fac236c-a0d8-0310-9745-ef116914eda1

--- python/fftfit_src/test_fftfit.py
+++ python/fftfit_src/test_fftfit.py
@@ -8,16 +8,16 @@
 #  ffft(d,npts,isign,ireal)
 #  fftfit(prof,s,phi,nmax,shift,eshift,snr,esnr,b,errb,ngood)
 
-from numpy import fmod, arange
+import numpy as num
 from psr_utils import gaussian_profile, TWOPI
 from fftfit import cprof, fftfit
-from RandomArray import normal
+
 template = gaussian_profile(64, 0.5, 0.1)
 c,amp,pha = cprof(template)
 #pha.savespace()
 pha1 = pha[0]
-pha = fmod(pha-arange(1,len(pha)+1)*pha1,TWOPI)
+pha = num.fmod(pha-num.arange(1,len(pha)+1)*pha1,TWOPI)
 for phs in [0.1, 0.3, 0.7]:
-    prof = gaussian_profile(64, phs, 0.1)+normal(0.0, 1.0, 64)
+    prof = gaussian_profile(64, phs, 0.1)+num.random.standard_normal(64)
     shift,eshift,snr,esnr,b,errb,ngood = fftfit(prof,amp,pha)
     print "True phs = %f, measured phs = %f +/- %f" % (phs, shift/len(prof),eshift/len(prof))
