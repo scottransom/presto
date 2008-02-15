98a9a7d6d7751e2d3f0039ff3a50c5a65924ae4f
Scott Ransom <sransom@nrao.edu>
2008-02-15 19:11:31 +0000

    Fixed another 64-bit issue and changed tabs to spaces.
    git-svn-id: file://localhost/data1/git/presto_svn/trunk@1323 5fac236c-a0d8-0310-9745-ef116914eda1

--- lib/python/residuals.py
+++ lib/python/residuals.py
@@ -35,15 +35,15 @@ def read_residuals(filename="resid2.tmp"):
     r.prefit_phs = Num.zeros(r.numTOAs, 'd')
     for ii in range(r.numTOAs):
         struct = rf.fort_read("ddddddddd")
-	(r.bary_TOA[ii], 
-	 r.postfit_phs[ii],
-	 r.postfit_sec[ii], 
-	 r.orbit_phs[ii], 
-	 r.bary_freq[ii], 
-	 r.weight[ii], 
-	 r.uncertainty[ii], 
-	 r.prefit_phs[ii]) = (struct[0], struct[1], struct[2], struct[3], \
-	                      struct[4], struct[5], struct[6], struct[7])
+        (r.bary_TOA[ii], 
+         r.postfit_phs[ii],
+         r.postfit_sec[ii], 
+         r.orbit_phs[ii], 
+         r.bary_freq[ii], 
+         r.weight[ii], 
+         r.uncertainty[ii], 
+         r.prefit_phs[ii]) = (struct[0], struct[1], struct[2], struct[3], \
+                              struct[4], struct[5], struct[6], struct[7])
     rf.close()
     if not Num.nonzero(r.orbit_phs): del r.orbit_phs
     if not Num.nonzero(r.bary_freq): del r.bary_freq
@@ -55,9 +55,9 @@ def read_residuals(filename="resid2.tmp"):
 def read_residuals_64bit(filename="resid2.tmp"):
     r = residuals()
     rf = open(filename)
-    # Fortran format: 2 longs (Fortran crap) + 9 doubles = 80 bytes
+    # Fortran format: 2 8-byte ints (Fortran crap) + 9 doubles = 88 bytes
     rf.seek(0, 2)
-    r.numTOAs = rf.tell()/80
+    r.numTOAs = rf.tell()/88
     rf.seek(0, 0)
     r.bary_TOA = Num.zeros(r.numTOAs, 'd')
     r.postfit_phs = Num.zeros(r.numTOAs, 'd')
@@ -68,17 +68,17 @@ def read_residuals_64bit(filename="resid2.tmp"):
     r.uncertainty = Num.zeros(r.numTOAs, 'd')
     r.prefit_phs = Num.zeros(r.numTOAs, 'd')
     for ii in range(r.numTOAs):
-	block = rf.read(80)
-	struct = Num.fromstring(block, 'd')
-	(r.bary_TOA[ii], 
-	 r.postfit_phs[ii],
-	 r.postfit_sec[ii], 
-	 r.orbit_phs[ii], 
-	 r.bary_freq[ii], 
-	 r.weight[ii], 
-	 r.uncertainty[ii], 
-	 r.prefit_phs[ii]) = (struct[1], struct[2], struct[3], struct[4], \
-	                      struct[5], struct[6], struct[7], struct[8])
+        block = rf.read(88)
+        struct = Num.fromstring(block, 'd')
+        (r.bary_TOA[ii], 
+         r.postfit_phs[ii],
+         r.postfit_sec[ii], 
+         r.orbit_phs[ii], 
+         r.bary_freq[ii], 
+         r.weight[ii], 
+         r.uncertainty[ii], 
+         r.prefit_phs[ii]) = (struct[1], struct[2], struct[3], struct[4], \
+                              struct[5], struct[6], struct[7], struct[8])
     rf.close()
     if not Num.nonzero(r.orbit_phs): del r.orbit_phs
     if not Num.nonzero(r.bary_freq): del r.bary_freq
