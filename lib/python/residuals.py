b52edaaa62f4217568be7a3f0b2785160b295d70
Scott Ransom <sransom@nrao.edu>
2008-02-15 18:49:59 +0000

    Added ability to change the filename for the residuals file.
    git-svn-id: file://localhost/data1/git/presto_svn/trunk@1320 5fac236c-a0d8-0310-9745-ef116914eda1

--- lib/python/residuals.py
+++ lib/python/residuals.py
@@ -15,24 +15,24 @@
 #    --Prefit residual (seconds)
 
 import mIO
-import numpy as Numeric
+import numpy as Num
 
 class residuals:
     pass
 
-def read_residuals():
+def read_residuals(filename="resid2.tmp"):
     r = residuals()
-    rf = mIO.binary_file("resid2.tmp")
+    rf = mIO.binary_file(filename)
     # Fortran format: 2 longs (Fortran crap) + 9 doubles = 80 bytes
     r.numTOAs = rf.size()/80
-    r.bary_TOA = Numeric.zeros(r.numTOAs, 'd')
-    r.postfit_phs = Numeric.zeros(r.numTOAs, 'd')
-    r.postfit_sec = Numeric.zeros(r.numTOAs, 'd')
-    r.orbit_phs = Numeric.zeros(r.numTOAs, 'd')
-    r.bary_freq = Numeric.zeros(r.numTOAs, 'd')
-    r.weight = Numeric.zeros(r.numTOAs, 'd')
-    r.uncertainty = Numeric.zeros(r.numTOAs, 'd')
-    r.prefit_phs = Numeric.zeros(r.numTOAs, 'd')
+    r.bary_TOA = Num.zeros(r.numTOAs, 'd')
+    r.postfit_phs = Num.zeros(r.numTOAs, 'd')
+    r.postfit_sec = Num.zeros(r.numTOAs, 'd')
+    r.orbit_phs = Num.zeros(r.numTOAs, 'd')
+    r.bary_freq = Num.zeros(r.numTOAs, 'd')
+    r.weight = Num.zeros(r.numTOAs, 'd')
+    r.uncertainty = Num.zeros(r.numTOAs, 'd')
+    r.prefit_phs = Num.zeros(r.numTOAs, 'd')
     for ii in range(r.numTOAs):
         struct = rf.fort_read("ddddddddd")
 	(r.bary_TOA[ii], 
@@ -45,31 +45,31 @@ def read_residuals():
 	 r.prefit_phs[ii]) = (struct[0], struct[1], struct[2], struct[3], \
 	                      struct[4], struct[5], struct[6], struct[7])
     rf.close()
-    if not Numeric.nonzero(r.orbit_phs): del r.orbit_phs
-    if not Numeric.nonzero(r.bary_freq): del r.bary_freq
-    if not Numeric.nonzero(r.weight): del r.weight
+    if not Num.nonzero(r.orbit_phs): del r.orbit_phs
+    if not Num.nonzero(r.bary_freq): del r.bary_freq
+    if not Num.nonzero(r.weight): del r.weight
     r.prefit_sec = r.postfit_sec/r.postfit_phs*r.prefit_phs
     r.uncertainty *= 1.e-6 # Convert uncertainties in usec to sec
     return r
 
-def read_residuals_64bit():
+def read_residuals_64bit(filename="resid2.tmp"):
     r = residuals()
-    rf = open("resid2.tmp")
+    rf = open(filename)
     # Fortran format: 2 longs (Fortran crap) + 9 doubles = 80 bytes
     rf.seek(0, 2)
     r.numTOAs = rf.tell()/80
     rf.seek(0, 0)
-    r.bary_TOA = Numeric.zeros(r.numTOAs, 'd')
-    r.postfit_phs = Numeric.zeros(r.numTOAs, 'd')
-    r.postfit_sec = Numeric.zeros(r.numTOAs, 'd')
-    r.orbit_phs = Numeric.zeros(r.numTOAs, 'd')
-    r.bary_freq = Numeric.zeros(r.numTOAs, 'd')
-    r.weight = Numeric.zeros(r.numTOAs, 'd')
-    r.uncertainty = Numeric.zeros(r.numTOAs, 'd')
-    r.prefit_phs = Numeric.zeros(r.numTOAs, 'd')
+    r.bary_TOA = Num.zeros(r.numTOAs, 'd')
+    r.postfit_phs = Num.zeros(r.numTOAs, 'd')
+    r.postfit_sec = Num.zeros(r.numTOAs, 'd')
+    r.orbit_phs = Num.zeros(r.numTOAs, 'd')
+    r.bary_freq = Num.zeros(r.numTOAs, 'd')
+    r.weight = Num.zeros(r.numTOAs, 'd')
+    r.uncertainty = Num.zeros(r.numTOAs, 'd')
+    r.prefit_phs = Num.zeros(r.numTOAs, 'd')
     for ii in range(r.numTOAs):
 	block = rf.read(80)
-	struct = Numeric.fromstring(block, 'd')
+	struct = Num.fromstring(block, 'd')
 	(r.bary_TOA[ii], 
 	 r.postfit_phs[ii],
 	 r.postfit_sec[ii], 
@@ -80,9 +80,9 @@ def read_residuals_64bit():
 	 r.prefit_phs[ii]) = (struct[1], struct[2], struct[3], struct[4], \
 	                      struct[5], struct[6], struct[7], struct[8])
     rf.close()
-    if not Numeric.nonzero(r.orbit_phs): del r.orbit_phs
-    if not Numeric.nonzero(r.bary_freq): del r.bary_freq
-    if not Numeric.nonzero(r.weight): del r.weight
+    if not Num.nonzero(r.orbit_phs): del r.orbit_phs
+    if not Num.nonzero(r.bary_freq): del r.bary_freq
+    if not Num.nonzero(r.weight): del r.weight
     r.prefit_sec = r.postfit_sec/r.postfit_phs*r.prefit_phs
     r.uncertainty *= 1.e-6 # Convert uncertainties in usec to sec
     return r
