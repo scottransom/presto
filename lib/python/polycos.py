1902bcf489915ab902d602b1213038cb7d39550b
Scott Ransom <sransom@nrao.edu>
2008-05-23 21:09:46 +0000

    Made the polyco reader tolerant of "D" rather than "E" for the scientific notation exponent symbol.
    git-svn-id: file://localhost/data1/git/presto_svn/trunk@1349 5fac236c-a0d8-0310-9745-ef116914eda1

--- lib/python/polycos.py
+++ lib/python/polycos.py
@@ -34,9 +34,9 @@ class polyco:
             self.coeffs = Num.zeros(self.numcoeff, 'd')
             for linenum in range(self.numcoeff/3):
                 sl = fileptr.readline().split()
-                self.coeffs[linenum*3+0] = float(sl[0])
-                self.coeffs[linenum*3+1] = float(sl[1])
-                self.coeffs[linenum*3+2] = float(sl[2])
+                self.coeffs[linenum*3+0] = float(sl[0].replace('D', 'E'))
+                self.coeffs[linenum*3+1] = float(sl[1].replace('D', 'E'))
+                self.coeffs[linenum*3+2] = float(sl[2].replace('D', 'E'))
     def phase(self, mjdi, mjdf):
         """
         self.phase(mjdi, mjdf):
