babe9e95fa535d45a657f0e75e0dbfa164f9573e
Scott Ransom <sransom@nrao.edu>
2009-12-03 21:23:00 -0500

    Fixed T0 calculation for the ELL1 model

--- lib/python/parfile.py
+++ lib/python/parfile.py
@@ -138,9 +138,10 @@ class psr_par:
                 setattr(self, 'P1', pd) 
         if hasattr(self, 'EPS1') and hasattr(self, 'EPS2'):
             ecc = math.sqrt(self.EPS1 * self.EPS1 + self.EPS2 * self.EPS2)
-            omega = math.atan2(self.EPS1, self.EPS2) * pu.RADTODEG
+            omega = math.atan2(self.EPS1, self.EPS2)
             setattr(self, 'E', ecc)
-            setattr(self, 'OM', omega)
+            setattr(self, 'OM', omega * pu.RADTODEG)
+            setattr(self, 'T0', self.TASC + self.PB * omega/pu.TWOPI)
         if hasattr(self, 'PB') and hasattr(self, 'A1') and not hasattr(self, 'E'):
             setattr(self, 'E', 0.0)
         pf.close()
