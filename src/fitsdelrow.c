6f0296122d3f617e55fe5d65da19e1d3bdf1654c
Scott Ransom <sransom@nrao.edu>
2010-09-23 10:57:52 -0400

    Added fitsdelrow usage and Makefile stuff for it

--- src/fitsdelrow.c
+++ src/fitsdelrow.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
 #include "fitsio.h"
-// v2
+
 int main(int argc, char *argv[])
 {
     fitsfile *infptr;   /* FITS file pointers defined in fitsio.h */
-    int status = 0, ii = 1;       /* status must always be initialized = 0  */
+    int status = 0;     /* status must always be initialized = 0  */
 
-    /* Open the input file */
-    if ( !fits_open_file(&infptr, argv[1], READWRITE, &status) )
-    {
-        /* Delete the first 7 rows */
-      fits_delete_rows(infptr, atoi(argv[2]), atoi(argv[3]), &status);
-	/* Close the input file */
-        fits_close_file(infptr, &status);
+    if (argc==1) {
+        printf("usage:  fitsdelrow fitsfile.fits[3] 1 7\n"
+               "        where the optional [#] means the HDU number, and 1 and 7\n"
+               "        are the start and ending rows inclusive to delete\n\n");
+        exit(0);
     }
 
+    /* Open the input file */
+    if (!fits_open_file(&infptr, argv[1], READWRITE, &status)) {
+        /* Delete the first 7 rows */
+        fits_delete_rows(infptr, atoi(argv[2]), atoi(argv[3]), &status);
+        /* Close the input file */
+        fits_close_file(infptr, &status);
+    }
+    
     /* if error occured, print out error message */
     if (status) fits_report_error(stderr, status);
     return(status);
