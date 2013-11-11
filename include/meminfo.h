0bc64f63a63f5b9266bb42577c1e8e257977a967
Scott Ransom <sransom@nrao.edu>
2013-11-11 22:06:36 +0800

    Set RAM size to 2GB/core for accelsearch

--- include/meminfo.h
+++ include/meminfo.h
@@ -1,5 +1,5 @@
 // The most RAM for a single core to use in accelsearch
-#define MAXRAMUSE             2000000
+#define MAXRAMUSE             2000000000
 // The longest length FFT to do in core memory
 #define MAXREALFFT            1000000000
 // The biggest length FFT to use FFTW for (if larger, use 6-step)
