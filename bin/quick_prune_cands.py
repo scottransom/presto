3a6b94d0fb4aedeada397a479dc3dc722513da1d
Scott Ransom <sransom@nrao.edu>
2014-04-26 12:11:37 -0400

    Added small script that removes likely bogus candidates from a single (text) ACCEL file

---
+++ bin/quick_prune_cands.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import sifting, sys
+
+if len(sys.argv) < 2:
+    sys.stderr.write("\nusage:  quick_prune_cands.py ACCEL_file_name [sigma]\n\n")
+    sys.exit()
+
+if len(sys.argv)==3:
+    sifting.sigma_threshold = float(sys.argv[2])
+
+cands = sifting.read_candidates([sys.argv[1]], track=True)
+cands.print_cand_summary()
+cands.to_file()
