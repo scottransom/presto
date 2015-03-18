0e6cca04cd2bdd62f3736442ba00a66a35a55aa4
Paul Ray <paul.ray@nrl.navy.mil>
2015-03-18 10:41:07 -0400

    Added command line program to choose good numout values

---
+++ bin/chooseN.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+from __future__ import (print_function,division)
+import psr_utils as pu
+import sys
+from infodata import infodata
+
+if len(sys.argv) != 2:
+    print("chooseN <file.inf|numpoints>")
+    print("    Prints a good value for fast FFTs to be used for -numout in prepdata/prepsubband")
+    sys.exit(1)
+
+if (sys.argv[1].endswith('.inf')):
+    inf = infodata(sys.argv[1])
+    n = inf.N
+else:
+    try:
+        n = int(sys.argv[1])
+    except:
+        print("chooseN <file.inf|numpoints>")
+        print("    Prints a good value for fast FFTs to be used for -numout in prepdata/prepsubband")
+        sys.exit(2)
+
+print(pu.choose_N(n))
