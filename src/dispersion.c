163fe7dacee9a9ffab0d2cdab63b9c44e2be0550
Scott Ransom <sransom@nrao.edu>
2015-04-24 00:40:06 -0700

    Use FFTW transpose to speedup subband dedispersion

--- src/dispersion.c
+++ src/dispersion.c
@@ -166,35 +166,37 @@ void dedisp_subbands(float *data, float *lastdata,
                      int *delays, int numsubbands, float *result)
 // De-disperse a stretch of data with numpts * numchan points into
 // numsubbands subbands.  Each time point for each subband is a float
-// in the result array.  The result array order is subbands of
-// increasing frequency together at each time pt.  The delays (in
+// in the result array.  The result array order is all the times for
+// each subband, starting with lowest freq subband.  The delays (in
 // bins) are in delays for each channel.  The input data and
 // dispdelays are always in ascending frequency order.  Input data are
-// ordered in time, with the channels stored together at each time
-// point.
+// contiguous channels with all of their points in time, starting with
+// the lowest freq channel.
 {
     const int chan_per_subband = numchan / numsubbands;
-    int ii;
+    int ii, jj;
 
     /* Initialize the result array */
     for (ii = 0; ii < numpts * numsubbands; ii++)
         result[ii] = 0.0;
 
     /* De-disperse into the subbands */
+    for (ii = 0; ii < numchan; ii++) {
+        const int subnum = ii / chan_per_subband;
+        const int dind = delays[ii];
+        float *sub = result + subnum * numpts;
+        float *chan = lastdata + ii * numpts + dind;
 #ifdef _OPENMP
-#pragma omp parallel for default(shared) private(ii)
+#pragma omp parallel for default(none) private(jj) shared(chan,numpts)
 #endif
-    for (ii = 0; ii < numsubbands; ii++) {
-        int jj, kk, ll;
-        int chan = ii * chan_per_subband;
-        for (jj = 0; jj < chan_per_subband; jj++, chan++) {
-            kk = chan + delays[chan] * numchan;
-            for (ll = 0; ll < numpts - delays[chan] ; ll++, kk += numchan)
-                result[ll * numsubbands + ii] += lastdata[kk];
-            kk = chan;
-            for (; ll < numpts ; ll++, kk += numchan)
-                result[ll * numsubbands + ii] += data[kk];
-        }
+        for (jj = 0; jj < numpts - dind; jj++)
+            sub[jj] += chan[jj];
+        chan = data + ii * numpts;
+#ifdef _OPENMP
+#pragma omp parallel for default(none) private(jj) shared(chan,numpts)
+#endif
+        for (jj = numpts - dind; jj < numpts; jj++)
+            sub[jj] += chan[jj];
     }
 }
 
