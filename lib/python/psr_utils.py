48c9648aa010ba29be87c197c7fa870bac906e33
Scott Ransom <sransom@nrao.edu>
2015-11-30 15:40:22 -0500

    Added Nice and Taylor 1995 galactic acceleration eqns

--- lib/python/psr_utils.py
+++ lib/python/psr_utils.py
@@ -528,9 +528,9 @@ def shklovskii_effect(pm, D):
     """
     return (pm/1000.0*ARCSECTORAD/SECPERJULYR)**2.0 * KMPERKPC*D / (C/1000.0)
 
-def galactic_accel_simple(l, b, D):
+def galactic_accel_simple(l, b, D, v_o=220.0, R_o = 8.0):
     """
-    galactic_accel_simple(l, b, D):
+    galactic_accel_simple(l, b, D, v_o=220.0, R_o = 8.0):
         Return the approximate projected acceleration/c (in s^-1)
         (a_p - a_ssb) dot n / c, where a_p and a_ssb are acceleration
         vectors, and n is the los vector.  This assumes a simple spherically
@@ -539,12 +539,42 @@ def galactic_accel_simple(l, b, D):
         b are the galactic longitude and latitude (in deg) respectively,
         and D is the distance in kpc.  This is eqn 2.4 of Phinney 1992.
     """
-    v_o = 220.0 # km/s
-    R_o = 8.0 # kpc
     A_sun = v_o*v_o / (C/1000.0 * R_o*KMPERKPC)
     d = D/R_o
     cbcl = Num.cos(b*DEGTORAD) * Num.cos(l*DEGTORAD)
     return -A_sun * (cbcl + (d - cbcl) / (1.0 + d*d - 2.0*d*cbcl))
+
+def galactic_accel(l, b, D, v_o=220.0, R_o = 8.0):
+    """
+    galactic_accel(l, b, D, v_o=220.0, R_o = 8.0):
+        Return the approximate projected acceleration/c (in s^-1)
+        (a_p - a_ssb) dot n / c, where a_p and a_ssb are acceleration
+        vectors, and n is the los vector.  This assumes v_o = 220 km/s
+        circular velocity and R_o = 8 kpc to the center of Galaxy.  l and
+        b are the galactic longitude and latitude (in deg) respectively,
+        and D is the distance in kpc.  This is eqn 5 of Nice & Taylor 1995.
+    """
+    A_sun = v_o*v_o / (C/1000.0 * R_o*KMPERKPC)
+    cb = Num.cos(b*DEGTORAD)
+    cl = Num.cos(l*DEGTORAD)
+    sl = Num.sin(l*DEGTORAD)
+    beta = D/R_o * cb - cl
+    return -A_sun * cb * (cl + beta / (sl**2 + beta**2))
+
+def gal_z_accel(l, b, D):
+    """
+    gal_z_accel(l, b, D):
+        Return the approximate projected acceleration/c (in s^-1)
+        (a_p - a_ssb) dot n / c, where a_p and a_ssb are acceleration
+        vectors, and n is the los vector, caused by the acceleration
+        of the pulsar towards the plane of the galaxy.  l and b are
+        the galactic longitude and latitude (in deg) respectively, and D
+        is the distance in kpc.  This is eqn 3+4 of Nice & Taylor 1995.
+    """
+    sb = Num.sin(b*DEGTORAD)
+    z = D * sb
+    az = 1.08e-19 * (1.25 * z / Num.sqrt(z**2 + 0.0324) + 0.58 * z)
+    return az * sb
 
 def beam_halfwidth(obs_freq, dish_diam):
     """
