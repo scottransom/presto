8c97d65bfb4e3384ed315f62c9ba3a42eccb770d
Scott Ransom <sransom@nrao.edu>
2017-09-12 14:27:48 -0400

    Added weighted mean and stdev calc

--- lib/python/psr_utils.py
+++ lib/python/psr_utils.py
@@ -123,6 +123,66 @@ def KS_test(data, cumdist, output=0):
         print "Prob the data is from the specified distrbution   (P) = %.3g" % P
     return (D, P)
 
+def weighted_mean(arrin, weights_in, inputmean=None, calcerr=False, sdev=False):
+    """
+    NAME:
+      weighted_mean()
+
+    PURPOSE:
+      Calculate the weighted mean, error, and optionally standard deviation of
+      an input array.  By default error is calculated assuming the weights are
+      1/err^2, but if you send calcerr=True this assumption is dropped and the
+      error is determined from the weighted scatter.
+
+    CALLING SEQUENCE:
+     wmean,werr = wmom(arr, weights, inputmean=None, calcerr=False, sdev=False)
+
+    INPUTS:
+      arr: A numpy array or a sequence that can be converted.
+      weights: A set of weights for each elements in array.
+    OPTIONAL INPUTS:
+      inputmean:
+          An input mean value, around which the mean is calculated.
+      calcerr=False:
+          Calculate the weighted error.  By default the error is calculated as
+          1/sqrt( weights.sum() ).  If calcerr=True it is calculated as sqrt(
+          (w**2 * (arr-mean)**2).sum() )/weights.sum()
+      sdev=False:
+          If True, also return the weighted standard deviation as a third
+          element in the tuple.
+
+    OUTPUTS:
+      wmean, werr: A tuple of the weighted mean and error. If sdev=True the
+         tuple will also contain sdev: wmean,werr,wsdev
+
+    REVISION HISTORY:
+      Converted from IDL: 2006-10-23. Erin Sheldon, NYU
+
+   """
+    # no copy made if they are already arrays
+    arr = Num.array(arrin, ndmin=1, copy=False)
+    # Weights is forced to be type double. All resulting calculations
+    # will also be double
+    weights = Num.array(weights_in, ndmin=1, dtype='f8', copy=False)
+    wtot = weights.sum()
+    # user has input a mean value
+    if inputmean is None:
+        wmean = ( weights*arr ).sum()/wtot
+    else:
+        wmean=float(inputmean)
+    # how should error be calculated?
+    if calcerr:
+        werr2 = ( weights**2 * (arr-wmean)**2 ).sum()
+        werr = Num.sqrt( werr2 )/wtot
+    else:
+        werr = 1.0/Num.sqrt(wtot)
+    # should output include the weighted standard deviation?
+    if sdev:
+        wvar = ( weights*(arr-wmean)**2 ).sum()/wtot
+        wsdev = Num.sqrt(wvar)
+        return wmean,werr,wsdev
+    else:
+        return wmean,werr
 
 def MJD_to_JD(MJD):
     """
