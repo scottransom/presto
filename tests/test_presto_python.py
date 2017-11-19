Fixed inconsistent omega units, changed Shapiro calcs to account for OMDOT

tests/test_presto_python.py
@@ -89,7 +89,7 @@ orb.p = 10000.0
 orb.e = 0.1
 orb.x = 1.0
 orb.t = 1234.0
-orb.w = np.radians(75.0)
+orb.w = 75.0
 orb.pd = orb.wd = 0.0
 E0 = presto.keplers_eqn(orb.t+0.0, orb.p, orb.e, 1e-15)
 E1 = presto.keplers_eqn(orb.t+100.0, orb.p, orb.e, 1e-15)
@@ -105,8 +105,6 @@ presto.E_to_v(Es, orb)
 Vs_check = np.asarray([-112.15558594, -122.45299212,
                        -131.9991447, -140.76659065])
 assert(np.allclose(Es, Vs_check))
-print "\nTODO:  fix inconsistent omega units of deg vs rad!!"
-orb.w = 75.0
 minv, maxv = presto.binary_velocity(300.0, orb)
 minv *= presto.SOL/1000.0
 maxv *= presto.SOL/1000.0
@@ -138,7 +136,7 @@ if 0:
 assert(rs[nn/2]==0.0)
 assert(pr[nn/2]==1.0)
 assert(round(pz[nn/2]-0.227675, 6)==0)
-assert(round(pw[nn/2]-0.019467, 6)==0)
+assert(round(pw[nn/2]-0.019462, 6)==0)
 print "success"
 
 print "Testing angle functions...",