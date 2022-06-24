09063e194de8dd443dc08d263e2171e9ea2565de
Kevin Luke <78250732+lukekevin@users.noreply.github.com>
2022-06-24 21:47:04 +0530

    Update weights_to_ignorechan.py
    added textfile generation for ignore channels

--- bin/weights_to_ignorechan.py
+++ bin/weights_to_ignorechan.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import numpy as np
 import sys
-
+import optparse
 def read_weights(filename):
     """
     reat_weights(filename):
@@ -36,7 +36,9 @@ def build_chanline(weights):
         ii += 1
     if outstr[-1]==':':
         outstr += str(N-1)
+    
     return outstr if outstr[-1]!=',' else outstr[:-1]
+
 
 def build_pazline(chanline):
     """
@@ -49,19 +51,28 @@ def build_pazline(chanline):
             in part else "-z "+part+" "
     return outstr
 
+
+
 if __name__=="__main__":
-
-    if len(sys.argv) != 2:
-        print("\nusage: {} file\n".format(sys.argv[0]))
-        sys.exit(1)
-
+  
+    parser = optparse.OptionParser()
+    parser.add_option('--file', type=str,
+                      help='The weights filename')
+    parser.add_option('--text', default=False, 
+                      action='store_true',help='The ignorechannels will be stored in a textfile')
+    options,args = parser.parse_args()
+    
     # Read the channels and weights
-    chans, weights = read_weights(sys.argv[1])
+    chans, weights = read_weights(options.file)
 
     # Get the chanline
     chanline = build_chanline(weights)
     print(chanline)
-
+    
+    if options.text:
+        with open('ignorechan.txt', 'w') as ignorechan:
+            ignorechan.write(chanline)
+    
     # Convert it to a paz command
     pazline = build_pazline(chanline)
     print("\n"+pazline)
