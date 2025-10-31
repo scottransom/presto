0218be010484b134967a1b855af952a977117c31
Scott Ransom <sransom@nrao.edu>
2025-10-31 14:17:45 -0400

    Fixed import issues and added test for cosine_rand.json as per #214

--- python/presto/events.py
+++ python/presto/events.py
@@ -2,7 +2,7 @@ import bisect
 from presto.psr_constants import PI, TWOPI, PIBYTWO
 from presto.simple_roots import newton_raphson
 from scipy.special import iv, chdtri, ndtr, ndtri
-from presto.cosine_rand import *
+from presto.cosine_rand import cosine_rand
 import numpy as np
 
 
@@ -14,14 +14,15 @@ def sine_events(pulsed_frac, Nevents, phase=0.0):
        'pulsed_frac', a phase offset 'phase', and with a
        sinusoidal pulse profile.
     """
-    Nsrc = int(pulsed_frac*Nevents+0.5)
+    Nsrc = int(pulsed_frac * Nevents + 0.5)
     Nbak = Nevents - Nsrc
-    phases = Num.zeros(Nevents, dtype=float)
+    phases = np.zeros(Nevents, dtype=float)
     phases[:Nsrc] += cosine_rand(Nsrc) + phase
-    phases[Nsrc:] += Num.random.random(Nbak)
-    phases = Num.fmod(phases, 1.0)
-    phases[phases<0.0] += 1.0
+    phases[Nsrc:] += np.random.random(Nbak)
+    phases = np.fmod(phases, 1.0)
+    phases[phases < 0.0] += 1.0
     return phases
+
 
 def gaussian_events(pulsed_frac, Nevents, fwhm, phase=0.0):
     """
@@ -32,14 +33,15 @@ def gaussian_events(pulsed_frac, Nevents, fwhm, phase=0.0):
        gaussian pulse profile of width 'fwhm'
     """
     sigma = fwhm / 2.35482
-    Nsrc = int(pulsed_frac*Nevents+0.5)
+    Nsrc = int(pulsed_frac * Nevents + 0.5)
     Nbak = Nevents - Nsrc
-    phases = Num.zeros(Nevents, dtype=float)
-    phases[:Nsrc] += Num.random.standard_normal(Nsrc)*sigma + phase
-    phases[Nsrc:] += Num.random.random(Nbak)
-    phases = Num.fmod(phases, 1.0)
-    phases[phases<0.0] += 1.0
+    phases = np.zeros(Nevents, dtype=float)
+    phases[:Nsrc] += np.random.standard_normal(Nsrc) * sigma + phase
+    phases[Nsrc:] += np.random.random(Nbak)
+    phases = np.fmod(phases, 1.0)
+    phases[phases < 0.0] += 1.0
     return phases
+
 
 def harm_to_sum(fwhm):
     """
@@ -47,12 +49,14 @@ def harm_to_sum(fwhm):
        For an MVMD profile of width 'fwhm', returns the
        optimal number of harmonics to sum incoherently
     """
+    # fmt: off
     fwhms = [0.0108, 0.0110, 0.0113, 0.0117, 0.0119, 0.0124, 0.0127, 0.0132,
              0.0134, 0.0140, 0.0145, 0.0151, 0.0154, 0.0160, 0.0167, 0.0173,
              0.0180, 0.0191, 0.0199, 0.0207, 0.0220, 0.0228, 0.0242, 0.0257,
              0.0273, 0.0295, 0.0313, 0.0338, 0.0366, 0.0396, 0.0437, 0.0482,
              0.0542, 0.0622, 0.0714, 0.0836, 0.1037, 0.1313, 0.1799, 0.2883]
     return len(fwhms)-bisect.bisect(fwhms, fwhm)+1
+
 
 def DFTexact(times, f, maxnumharms=20):
     """
@@ -61,8 +65,9 @@ def DFTexact(times, f, maxnumharms=20):
        corresponding to the harmonics of the 'times' (in sec)
        with a fundamental at frequency 'f' Hz.
     """
-    const = -TWOPI*(Num.arange(maxnumharms, dtype=float)+1.0)*f*complex(0.0, 1.0)
-    return Num.add.reduce(Num.exp(Num.outerproduct(const,times)), axis=1)
+    const = -TWOPI * (np.arange(maxnumharms, dtype=float) + 1.0) * f * complex(0.0, 1.0)
+    return np.add.reduce(np.exp(np.outerproduct(const, times)), axis=1)
+
 
 def incoherent_sum(amps):
     """
@@ -71,7 +76,8 @@ def incoherent_sum(amps):
        amplitudes.  Usually these correspond to the complex
        harmonics of a periodic signal.
     """
-    return Num.add.accumulate(Num.abs(amps)**2.0)
+    return np.add.accumulate(np.abs(amps) ** 2.0)
+
 
 def coherent_sum(amps):
     """
@@ -80,12 +86,12 @@ def coherent_sum(amps):
        of an array of complex Fourier amplitudes.  Usually these
        correspond to the complex harmonics of a periodic signal.
     """
-    phss = Num.arctan2(amps.imag, amps.real)
+    phss = np.arctan2(amps.imag, amps.real)
     phs0 = phss[0]
-    phscorr = phs0 - Num.fmod(Num.arange(1.0, len(amps)+1,
-                                         dtype=float)*phs0, TWOPI)
-    sumamps = Num.add.accumulate(amps*Num.exp(complex(0.0, 1.0)*phscorr))
-    return Num.abs(sumamps)**2.0
+    phscorr = phs0 - np.fmod(np.arange(1.0, len(amps) + 1, dtype=float) * phs0, TWOPI)
+    sumamps = np.add.accumulate(amps * np.exp(complex(0.0, 1.0) * phscorr))
+    return np.abs(sumamps) ** 2.0
+
 
 def Htest_exact(phases, maxnumharms=20, weights=None):
     """
@@ -101,18 +107,20 @@ def Htest_exact(phases, maxnumharms=20, weights=None):
     """
     N = len(phases)
     Zm2s = np.zeros(maxnumharms, dtype=np.float)
-    rad_phases = 2.0*np.pi*phases
-    weightfact = 1.0/(np.sum(weights**2.0) / N) if \
-                 weights is not None else 1.0
-    for harmnum in range(1, maxnumharms+1):
-        phss = harmnum*rad_phases
-        Zm2s[harmnum-1] = 2.0/N*(np.add.reduce(np.sin(phss))**2.0+
-                                 np.add.reduce(np.cos(phss))**2.0)
-        Zm2s[harmnum-1] *= weightfact
-    hs = np.add.accumulate(Zm2s) - \
-         4.0*np.arange(1.0, maxnumharms+1)+4.0
+    rad_phases = 2.0 * np.pi * phases
+    weightfact = 1.0 / (np.sum(weights**2.0) / N) if weights is not None else 1.0
+    for harmnum in range(1, maxnumharms + 1):
+        phss = harmnum * rad_phases
+        Zm2s[harmnum - 1] = (
+            2.0
+            / N
+            * (np.add.reduce(np.sin(phss)) ** 2.0 + np.add.reduce(np.cos(phss)) ** 2.0)
+        )
+        Zm2s[harmnum - 1] *= weightfact
+    hs = np.add.accumulate(Zm2s) - 4.0 * np.arange(1.0, maxnumharms + 1) + 4.0
     bestharm = hs.argmax()
-    return (hs[bestharm], bestharm+1)
+    return (hs[bestharm], bestharm + 1)
+
 
 def Hstat_prob(h):
     """
@@ -120,7 +128,8 @@ def Hstat_prob(h):
        Return the probability associated with an H-test statistic
        of value 'h'.  Uses de Jager & Busching 2010 result.
     """
-    return Num.exp(-0.4 * h)
+    return np.exp(-0.4 * h)
+
 
 def gauss_sigma_to_prob(sigma):
     """
@@ -133,10 +142,20 @@ def gauss_sigma_to_prob(sigma):
     else:
         # From A&S page 932, eqn 26.2.12 for Q(x)
         x = sigma
-        Z = 1.0/Num.sqrt(2.0*Num.pi) * Num.exp(-0.5*x*x)
-        series = Num.sum(Num.asarray([1.0, -1.0/(x*x), 3.0/(x**4.0),
-                                      -15.0/(x**6.0), 105.0/(x**8.0)]))
-        return Z/x*series
+        Z = 1.0 / np.sqrt(2.0 * np.pi) * np.exp(-0.5 * x * x)
+        series = np.sum(
+            np.asarray(
+                [
+                    1.0,
+                    -1.0 / (x * x),
+                    3.0 / (x**4.0),
+                    -15.0 / (x**6.0),
+                    105.0 / (x**8.0),
+                ]
+            )
+        )
+        return Z / x * series
+
 
 def prob_to_gauss_sigma(prob):
     """
@@ -147,8 +166,10 @@ def prob_to_gauss_sigma(prob):
     """
     return ndtri(prob)
 
-def xray_time_to_detect(ctrate, pfract, dt, fpsr, bins=0, confidence=0.99,
-                        detectfract=0.99):
+
+def xray_time_to_detect(
+    ctrate, pfract, dt, fpsr, bins=0, confidence=0.99, detectfract=0.99
+):
     """
     xray_time_to_detect(ctrate, pfract, dt, fpsr, bins=0, confidence=0.99,
                         detectfract=0.99):
@@ -165,19 +186,21 @@ def xray_time_to_detect(ctrate, pfract, dt, fpsr, bins=0, confidence=0.99,
         would detect the specified signal at 'confidence' level).
     """
     nyquist_freq = 0.5 / dt
-    factor = binning_factor(fpsr, nyquist_freq)**2.0
+    factor = binning_factor(fpsr, nyquist_freq) ** 2.0
     A = pfract * ctrate  # Signal ct rate
-    if (bins):
+    if bins:
         P_detect = max_noise_power(bins, confidence=confidence)
         power_required = required_signal_power(P_detect, confidence=detectfract)
-	# The following is from para 1, sect 3.3, of Ransom, Gaensler, and Slane, 2002
-	#return  (power_required - 1.0)          4 * ctrate * dt**2.0 / (A**2.0 * factor) * 
+    # The following is from para 1, sect 3.3, of Ransom, Gaensler, and Slane, 2002
+    # return  (power_required - 1.0)          4 * ctrate * dt**2.0 / (A**2.0 * factor) *
     else:
         print("Not implemented yet...I think we need to iterate.")
+
 
 # The following routines are based on the method of signal
 # estimation described by Vaughan et al., 1994, ApJ, 435, p362.
 # The math comes from Groth, 1975, ApJS, 29, p285.
+
 
 def power_average(signal_power, n=1):
     """
@@ -188,6 +211,7 @@ def power_average(signal_power, n=1):
     """
     return signal_power + n
 
+
 def power_variance(signal_power, n=1):
     """
     power_variance(signal_power, n=1):
@@ -197,6 +221,7 @@ def power_variance(signal_power, n=1):
     """
     return 2.0 * signal_power + n
 
+
 def power_sigma(signal_power, n=1):
     """
     power_sigma(signal_power, n=1):
@@ -204,7 +229,8 @@ def power_sigma(signal_power, n=1):
         given a signal with intrinsic power 'signal_power' and
         'n' summed powers.  This is from equation 14 in Groth, 1975.
     """
-    return Num.sqrt(power_variance(signal_power, n))
+    return np.sqrt(power_variance(signal_power, n))
+
 
 def log_fact_table(maxn):
     """
@@ -212,9 +238,10 @@ def log_fact_table(maxn):
         Return a table of the natural logarithms of the
         first 'maxn'+1 factorials.
     """
-    table = Num.arange(maxn+1, dtype='d')
+    table = np.arange(maxn + 1, dtype="d")
     table[0] = 1.0
-    return Num.add.accumulate(Num.log(table))
+    return np.add.accumulate(np.log(table))
+
 
 def binning_factor(freq, nyquist_freq):
     """
@@ -226,8 +253,9 @@ def binning_factor(freq, nyquist_freq):
         interest and 'nyquist_freq' is the Nyquist Frequency
         which can be defined as N/(2*T).
     """
-    x = 0.5 * Num.asarray(freq) / nyquist_freq
-    return Num.sinc(x)  # numpy sinc is defined with pi
+    x = 0.5 * np.asarray(freq) / nyquist_freq
+    return np.sinc(x)  # numpy sinc is defined with pi
+
 
 def max_noise_power(bins, n=1, confidence=0.99):
     """
@@ -239,10 +267,11 @@ def max_noise_power(bins, n=1, confidence=0.99):
         This is P_detect in Vaughan et. al, 1994, and is also
         known as P_threshold.
     """
-    if (n==1):
-        return -Num.log((1.0 - confidence) / bins)
+    if n == 1:
+        return -np.log((1.0 - confidence) / bins)
     else:
         return 0.5 * chdtri(2.0 * n, (1.0 - confidence) / bins)
+
 
 def prob_power_series(power, signal_power, n=1, TOL=1.0e-14):
     """
@@ -253,20 +282,23 @@ def prob_power_series(power, signal_power, n=1, TOL=1.0e-14):
         This method evaluates the integral using an infinite
         sum and is equation 16 in Groth, 1975.
     """
-    fact = Num.exp(-(power + signal_power))
+    fact = np.exp(-(power + signal_power))
     lf = log_fact_table((power + signal_power) * 5)
-    lp, lps = Num.log(power), Num.log(signal_power)
+    lp, lps = np.log(power), np.log(signal_power)
     sum = 0.0
     term = 1.0
     m = 0
-    while (1):
+    while 1:
         kmax = m + n
-        term = fact * Num.add.reduce(Num.exp((Num.arange(kmax)*lp + m*lps) - \
-                                             (lf[0:kmax] + lf[m])))
+        term = fact * np.add.reduce(
+            np.exp((np.arange(kmax) * lp + m * lps) - (lf[0:kmax] + lf[m]))
+        )
         sum = sum + term
-        if (m > signal_power and term < TOL):  break
+        if m > signal_power and term < TOL:
+            break
         m = m + 1
     return 1.0 - sum
+
 
 def prob_power_integral(power, signal_power, n=1):
     """
@@ -277,18 +309,23 @@ def prob_power_integral(power, signal_power, n=1):
         This method evaluates the integral numerically and
         is equation 18 in Groth, 1975.
     """
+
     def integrand(theta, p, ps, n):
         t1 = 2 * n * theta
-        t2 = Num.sin(2.0 * theta)
+        t2 = np.sin(2.0 * theta)
         A = t1 + ps * t2
         B = t1 + (ps - p) * t2
-        sintheta = Num.sin(theta)
+        sintheta = np.sin(theta)
         sin2theta = sintheta**2.0
-        return (Num.exp(-2.0 * ps * sin2theta) *
-                (Num.sin(A - theta) - Num.exp(-2.0 * p * sin2theta) *
-                 Num.sin(B - theta)) / sintheta)
+        return (
+            np.exp(-2.0 * ps * sin2theta)
+            * (np.sin(A - theta) - np.exp(-2.0 * p * sin2theta) * np.sin(B - theta))
+            / sintheta
+        )
+
     (val, err) = quad(integrand, 0.0, PIBYTWO, (power, signal_power, n))
-    return val/PI
+    return val / PI
+
 
 def power_probability(power, signal_power, n=1):
     """
@@ -299,9 +336,12 @@ def power_probability(power, signal_power, n=1):
         12 in Groth, 1975 and is the integrand of the
         prob_power_* functions (which integrate it from 0 to P)
     """
-    return (power / signal_power)**(0.5 * (n - 1)) * \
-           Num.exp(-(power + signal_power)) * \
-           iv(n - 1.0, 2 * Num.sqrt(power * signal_power))
+    return (
+        (power / signal_power) ** (0.5 * (n - 1))
+        * np.exp(-(power + signal_power))
+        * iv(n - 1.0, 2 * np.sqrt(power * signal_power))
+    )
+
 
 def required_signal_power(power, n=1, confidence=0.99):
     """
@@ -316,12 +356,16 @@ def required_signal_power(power, n=1, confidence=0.99):
         in the power spectrum.
     """
     prob = 1.0 - confidence
+
     def func(x, power=power, prob=prob, n=n):
         return prob_power_series(power, x, n) - prob
+
     def dfunc(x, power=power, n=n):
         return power_probability(power, x, n)
+
     P_signal = newton_raphson(func, dfunc, 0.0001, 100.0)
     return P_signal
+
 
 def fft_sensitivity(N, bins=0, n=1, confidence=0.99):
     """
@@ -338,9 +382,11 @@ def fft_sensitivity(N, bins=0, n=1, confidence=0.99):
         These calculations are based on the Vaughan et al 1994 paper
         and compute P_sens.
     """
-    if not (bins): bins = N / 2
+    if not (bins):
+        bins = N / 2
     P_threshold = max_noise_power(bins, n, confidence)
     return required_signal_power(P_threshold, n, confidence)
+
 
 def rzw_sensitivity(N, zlo=-100.0, zhi=100.0, n=1, confidence=0.99):
     """
@@ -359,6 +405,7 @@ def rzw_sensitivity(N, zlo=-100.0, zhi=100.0, n=1, confidence=0.99):
     P_threshold = max_noise_power(bins, n, confidence)
     return required_signal_power(P_threshold, n, confidence)
 
+
 def binned_fft_sensitivity(N, dt, freq, bins=0, n=1, confidence=0.99):
     """
     binned_fft_sensitivity(N, dt, freq, bins=0, n=1, confidence=0.99):
@@ -376,11 +423,11 @@ def binned_fft_sensitivity(N, dt, freq, bins=0, n=1, confidence=0.99):
         the Vaughan et al 1994 paper and compute P_sens.
     """
     nyquist_freq = 0.5 / dt
-    factor = binning_factor(freq, nyquist_freq)**2.0
+    factor = binning_factor(freq, nyquist_freq) ** 2.0
     return fft_sensitivity(N, bins, n, confidence) / factor
 
-def binned_rzw_sensitivity(N, dt, freq, zlo=-100.0, zhi=100.0,
-                           n=1, confidence=0.99):
+
+def binned_rzw_sensitivity(N, dt, freq, zlo=-100.0, zhi=100.0, n=1, confidence=0.99):
     """
     binned_rzw_sensitivity(N, dt, freq, zlo=-100.0, zhi=100.0,
                            n=1, confidence=0.99):
@@ -396,8 +443,9 @@ def binned_rzw_sensitivity(N, dt, freq, zlo=-100.0, zhi=100.0,
     """
     bins = N / 2.0 * (zhi - zlo + 1.0) / 6.95
     nyquist_freq = 0.5 / dt
-    factor = binning_factor(freq, nyquist_freq)**2.0
+    factor = binning_factor(freq, nyquist_freq) ** 2.0
     return fft_sensitivity(N, bins, n, confidence) / factor
+
 
 def pulsed_fraction_limit(Nphot, Pow):
     """
@@ -413,27 +461,28 @@ def pulsed_fraction_limit(Nphot, Pow):
         you should divide the returned value by the fraction of Nphot
         that actually comes from the _source_ (i.e. the NS).
     """
-    return Num.sqrt(4.0 * (Pow - 1.0) / Nphot)
+    return np.sqrt(4.0 * (Pow - 1.0) / Nphot)
 
 
-if __name__=="__main__":
-    from presto.psr_utils import *
-    from presto.Pgplot import *
-    from presto.presto import *
-    from RandomArray import *
+if __name__ == "__main__":
+    import presto.psr_utils as pu
+    import presto.presto as pp
+    from presto.Pgplot import plotxy, closeplot
 
-    prof = expcos_profile(128, 0.0, 0.1) + normal(0.0, 5.0, 128)
+    prof = pu.expcos_profile(128, 0.0, 0.1) + np.random.standard_normal(128)
     plotxy(prof)
     closeplot()
-    fprof = rfft(prof)
-    fprof = fprof/Num.sqrt(fprof[0].real)
-    pows = spectralpower(fprof)
-    tcsum =  Num.add.accumulate(Num.sqrt(pows[1:10]))**2.0
+    fprof = pp.rfft(prof)
+    fprof = fprof / np.sqrt(fprof[0].real)
+    pows = pp.spectralpower(fprof)
+    tcsum = np.add.accumulate(np.sqrt(pows[1:10])) ** 2.0
     csum = coherent_sum(fprof[1:10])
     isum = incoherent_sum(fprof[1:10])
     print(isum)
     print(csum)
     print(tcsum)
     for ii in range(len(csum)):
-        print(candidate_sigma(isum[ii], ii+1, 1), candidate_sigma(csum[ii]/(ii+1), 1, 1))
-        
+        print(
+            pp.candidate_sigma(float(isum[ii]), ii + 1, 1),
+            pp.candidate_sigma(csum[ii] / (ii + 1), 1, 1),
+        )
--- tests/test_presto_python.py
+++ tests/test_presto_python.py
@@ -1,59 +1,59 @@
-from __future__ import print_function
 import numpy as np
+import presto.events  # tests whether cosine_rand.json was installed correctly
 from presto import presto
 import os
 from os import path
 
 here = path.dirname(__file__)
 
-print("Testing FFT stuff...", end=' ')
+print("Testing FFT stuff...", end=" ")
 N = 20
 x = np.random.standard_normal(N)
 nx = presto.rfft(presto.rfft(x, -1), 1)
-assert(np.allclose(x, nx, atol=1e-6))
+assert np.allclose(x, nx, atol=1e-6)
 print("success")
 
-print("Testing FFTW call...", end=' ')
+print("Testing FFTW call...", end=" ")
 cx = np.random.standard_normal(N).astype(np.complex64)
 ncx = np.array(cx, copy=1)
 presto.fftwcall(cx, -1)
 presto.fftwcall(cx, 1)
-assert(np.allclose(cx/N, ncx, atol=1e-6))
+assert np.allclose(cx / N, ncx, atol=1e-6)
 print("success")
 
-print("Testing tablesixstepfft call...", end=' ')
+print("Testing tablesixstepfft call...", end=" ")
 cx = np.random.standard_normal(N).astype(np.complex64)
 ncx = np.array(cx, copy=1)
 presto.tablesixstepfft(cx, -1)
 presto.tablesixstepfft(cx, 1)
-assert(np.allclose(cx/N, ncx, atol=1e-6))
+assert np.allclose(cx / N, ncx, atol=1e-6)
 print("success")
 
-print("Testing reading infiles...", end=' ')
+print("Testing reading infiles...", end=" ")
 x = presto.read_inffile(path.join(here, "1937_DM71.02_zerodm.inf"), verbose=False)
-assert(x.telescope=="GBT")
-assert(x.mjd_i==55267)
-assert(x.dt==8.192e-05)
-assert(x.numonoff==1)
-assert(x.analyzer=="sransom")
+assert x.telescope == "GBT"
+assert x.mjd_i == 55267
+assert x.dt == 8.192e-05
+assert x.numonoff == 1
+assert x.analyzer == "sransom"
 print("success")
 
-print("Testing writing infiles...", end=' ')
-x.analyzer="test"
-x.name="xxx"
-x.dt=0.125
+print("Testing writing infiles...", end=" ")
+x.analyzer = "test"
+x.name = "xxx"
+x.dt = 0.125
 presto.write_inffile(x, verbose=False)
 y = presto.read_inffile("xxx", verbose=False)
-assert(y.analyzer=="test")
-assert(y.bary==0)
-assert(y.numonoff==1)
-assert(y.dt==0.125)
+assert y.analyzer == "test"
+assert y.bary == 0
+assert y.numonoff == 1
+assert y.dt == 0.125
 os.remove("xxx.inf")
 print("success")
 
 print("""Note: If the following fails, edit $PRESTO/meson.build and remove
         '-DUSE_FFTW_MALLOC' from the `_global_c_args` definition, and re-build.""")
-print("Testing allocation and freeing of memory...", end=' ')
+print("Testing allocation and freeing of memory...", end=" ")
 for ii in range(1024):
     a = presto.gen_fvect(1024 * 32768)
     del a
@@ -62,31 +62,32 @@ for ii in range(1024):
     del a
 print("success")
 
-print("Testing psrparams and orbitparams stuff...", end=' ')
+print("Testing psrparams and orbitparams stuff...", end=" ")
 psr = presto.psrepoch("J0737-3039A", 56000.0, verbose=False)
-assert(round(psr.dm-48.916, 7)==0)
+assert round(psr.dm - 48.916, 7) == 0
 # This needs to change when we start using the actual psrcat.db file
-assert(round(psr.orb.p-8834.534998272, 7)==0)
+assert round(psr.orb.p - 8834.534998272, 7) == 0
 print("success")
 
-print("Testing spectralpower and spectralphase...", end=' ')
+print("Testing spectralpower and spectralphase...", end=" ")
 a = np.arange(5.0) + complex(0.0, 1.0)
-assert(np.allclose(presto.spectralpower(a),
-                   np.arange(5.0)**2.0 + 1))
-assert(np.allclose(presto.spectralphase(a),
-                   np.array([90., 45., 26.56505203, 18.43494797, 14.03624344])))
+assert np.allclose(presto.spectralpower(a), np.arange(5.0) ** 2.0 + 1)
+assert np.allclose(
+    presto.spectralphase(a),
+    np.array([90.0, 45.0, 26.56505203, 18.43494797, 14.03624344]),
+)
 print("success")
 
-print("Testing vector shifting / rotation...", end=' ')
+print("Testing vector shifting / rotation...", end=" ")
 a = np.arange(4, dtype=np.float32)
 presto.frotate(a, 1)
-assert(np.allclose(a, np.array([1, 2, 3, 0])))
+assert np.allclose(a, np.array([1, 2, 3, 0]))
 a = np.arange(4, dtype=np.float64)
 presto.drotate(a, 1)
-assert(np.allclose(a, np.array([1, 2, 3, 0])))
+assert np.allclose(a, np.array([1, 2, 3, 0]))
 print("success")
 
-print("Testing orbit integration stuff...", end=' ')
+print("Testing orbit integration stuff...", end=" ")
 orb = presto.orbitparams()
 orb.p = 10000.0
 orb.e = 0.1
@@ -94,107 +95,115 @@ orb.x = 1.0
 orb.t = 1234.0
 orb.w = 75.0
 orb.pd = orb.wd = 0.0
-E0 = presto.keplers_eqn(orb.t+0.0, orb.p, orb.e, 1e-15)
-E1 = presto.keplers_eqn(orb.t+100.0, orb.p, orb.e, 1e-15)
-E2 = presto.keplers_eqn(orb.t+200.0, orb.p, orb.e, 1e-15)
-E3 = presto.keplers_eqn(orb.t+300.0, orb.p, orb.e, 1e-15)
+E0 = presto.keplers_eqn(orb.t + 0.0, orb.p, orb.e, 1e-15)
+E1 = presto.keplers_eqn(orb.t + 100.0, orb.p, orb.e, 1e-15)
+E2 = presto.keplers_eqn(orb.t + 200.0, orb.p, orb.e, 1e-15)
+E3 = presto.keplers_eqn(orb.t + 300.0, orb.p, orb.e, 1e-15)
 Es = np.asarray([E0, E1, E2, E3])
-Es_check = np.asarray([ 0.85050653, 0.9175909,
-                        0.9842971, 1.05061346])
-assert(np.allclose(Es, Es_check))
+Es_check = np.asarray([0.85050653, 0.9175909, 0.9842971, 1.05061346])
+assert np.allclose(Es, Es_check)
 Es_new = presto.dorbint(E0, 4, 100.0, orb)
-assert(np.allclose(Es_new, Es_check))
+assert np.allclose(Es_new, Es_check)
 presto.E_to_v(Es, orb)
-Vs_check = np.asarray([-112.15558594, -122.45299212,
-                       -131.9991447, -140.76659065])
-assert(np.allclose(Es, Vs_check))
+Vs_check = np.asarray([-112.15558594, -122.45299212, -131.9991447, -140.76659065])
+assert np.allclose(Es, Vs_check)
 minv, maxv = presto.binary_velocity(300.0, orb)
-minv *= presto.SOL/1000.0
-maxv *= presto.SOL/1000.0
-assert(round(minv-Vs_check.min(), 7)==0)
-assert(round(maxv-Vs_check.max(), 7)==0)
+minv *= presto.SOL / 1000.0
+maxv *= presto.SOL / 1000.0
+assert round(minv - Vs_check.min(), 7) == 0
+assert round(maxv - Vs_check.max(), 7) == 0
 print("success")
 
-print("Testing Fourier response generation...", end=' ')
+print("Testing Fourier response generation...", end=" ")
 numbetween = 16
 z = 5.0
 w = 40.0
-bins_per_side = max([presto.r_resp_halfwidth(presto.LOWACC), \
-                     presto.z_resp_halfwidth(z, presto.LOWACC), \
-                     presto.w_resp_halfwidth(z, w, presto.LOWACC)])
-nn = numbetween * bins_per_side * 2;
+bins_per_side = max(
+    [
+        presto.r_resp_halfwidth(presto.LOWACC),
+        presto.z_resp_halfwidth(z, presto.LOWACC),
+        presto.w_resp_halfwidth(z, w, presto.LOWACC),
+    ]
+)
+nn = numbetween * bins_per_side * 2
 rresp = presto.gen_r_response(0.0, numbetween, nn)
 zresp = presto.gen_z_response(0.0, numbetween, nn, z)
 wresp = presto.gen_w_response(0.0, numbetween, nn, z, w)
 pr = presto.spectralpower(rresp)
 pz = presto.spectralpower(zresp)
 pw = presto.spectralpower(wresp)
-rs = np.arange(float(nn))/numbetween - bins_per_side
+rs = np.arange(float(nn)) / numbetween - bins_per_side
 if False:
     import matplotlib.pyplot as plt
-    plt.plot(rs, pr, 'b-')
-    plt.plot(rs, pz, 'g-')
-    plt.plot(rs, pw, 'r-')
+
+    plt.plot(rs, pr, "b-")
+    plt.plot(rs, pz, "g-")
+    plt.plot(rs, pw, "r-")
     plt.show()
-assert(rs[nn//2]==0.0)
-assert(pr[nn//2]==1.0)
-assert(round(pz[nn//2]-0.227675, 6)==0)
-assert(round(pw[nn//2]-0.019462, 6)==0)
+assert rs[nn // 2] == 0.0
+assert pr[nn // 2] == 1.0
+assert round(pz[nn // 2] - 0.227675, 6) == 0
+assert round(pw[nn // 2] - 0.019462, 6) == 0
 print("success")
 
-print("Testing angle functions...", end=' ')
+print("Testing angle functions...", end=" ")
 dd1 = 15.25
-dd2 = presto.dms2rad(*presto.deg2dms(dd1))*presto.RADTODEG
-assert(round(dd1-dd2, 12)==0)
+dd2 = presto.dms2rad(*presto.deg2dms(dd1)) * presto.RADTODEG
+assert round(dd1 - dd2, 12) == 0
 dd1 = -0.5
-dd2 = presto.dms2rad(*presto.deg2dms(dd1))*presto.RADTODEG
-assert(round(dd1-dd2, 12)==0)
+dd2 = presto.dms2rad(*presto.deg2dms(dd1)) * presto.RADTODEG
+assert round(dd1 - dd2, 12) == 0
 hh1 = 12.125
-hh2 = presto.hms2rad(*presto.hours2hms(hh1))*presto.RADTODEG/15.0
-assert(round(hh1-hh2, 12)==0)
+hh2 = presto.hms2rad(*presto.hours2hms(hh1)) * presto.RADTODEG / 15.0
+assert round(hh1 - hh2, 12) == 0
 hh1 = -0.5
-hh2 = presto.hms2rad(*presto.hours2hms(hh1))*presto.RADTODEG/15.0
-assert(round(hh1-hh2, 12)==0)
-ang = presto.sphere_ang_diff(10.0*presto.DEGTORAD, 10.0*presto.DEGTORAD,
-                             50.0*presto.DEGTORAD, -10.0*presto.DEGTORAD)
-assert(round(160334.960*presto.ARCSEC2RAD-ang, 7)==0)
+hh2 = presto.hms2rad(*presto.hours2hms(hh1)) * presto.RADTODEG / 15.0
+assert round(hh1 - hh2, 12) == 0
+ang = presto.sphere_ang_diff(
+    10.0 * presto.DEGTORAD,
+    10.0 * presto.DEGTORAD,
+    50.0 * presto.DEGTORAD,
+    -10.0 * presto.DEGTORAD,
+)
+assert round(160334.960 * presto.ARCSEC2RAD - ang, 7) == 0
 print("success")
 
 # Only run this test if TEMPO is available
 envval = os.getenv("TEMPO")
 if envval is not None:
-    print("Testing get_baryv (barycenter)...", end=' ')
-    vavg1 = presto.get_baryv("18:24:32.9520", "-24:52:12.0000",
-                             56421.44222222222222, 214.5386496, obs="GB")
+    print("Testing get_baryv (barycenter)...", end=" ")
+    vavg1 = presto.get_baryv(
+        "18:24:32.9520", "-24:52:12.0000", 56421.44222222222222, 214.5386496, obs="GB"
+    )
     vavg2 = -7.2069293455783169e-05
-    assert(round(vavg1-vavg2, 10)==0)
+    assert round(vavg1 - vavg2, 10) == 0
     print("success")
 else:
     print("Skipping test of presto.get_baryv() since TEMPO not set.")
 
-print("Testing simple folding code...", end=' ')
+print("Testing simple folding code...", end=" ")
 prof, phs = presto.fold(np.ones(10000), 0.001, 10, 1)
-assert(np.allclose(prof, prof.mean()))
-assert(np.all(prof>0))
+assert np.allclose(prof, prof.mean())
+assert np.all(prof > 0)
 prof, phs = presto.fold(np.ones(10000), 0.001, 100, 1)
-assert(np.allclose(prof, prof.mean()))
-assert(np.all(prof>0))
+assert np.allclose(prof, prof.mean())
+assert np.all(prof > 0)
 prof, phs = presto.fold(np.ones(10000), 0.001, 200, 1)
-assert(np.allclose(prof, prof.mean()))
-assert(np.all(prof>0))
+assert np.allclose(prof, prof.mean())
+assert np.all(prof > 0)
 prof, phs = presto.fold(np.ones(10000), 0.001, 500, 1)
-assert(np.allclose(prof, prof.mean()))
-assert(np.all(prof>0))
+assert np.allclose(prof, prof.mean())
+assert np.all(prof > 0)
 prof, phs = presto.fold(np.ones(10000), 0.001, 100, 1, tlo=-1.12313)
-assert(np.allclose(prof, prof.mean()))
-assert(np.all(prof>0))
+assert np.allclose(prof, prof.mean())
+assert np.all(prof > 0)
 prof, phs = presto.fold(np.ones(10000), 0.001, 100, 1, startphs=0.65765)
-assert(np.allclose(prof, prof.mean()))
-assert(np.all(prof>0))
+assert np.allclose(prof, prof.mean())
+assert np.all(prof > 0)
 prof, phs = presto.fold(np.ones(10000), 0.001, 100, 1, startphs=0.99)
-assert(np.allclose(prof, prof.mean()))
-assert(np.all(prof>0))
+assert np.allclose(prof, prof.mean())
+assert np.all(prof > 0)
 prof, phs = presto.fold(np.ones(10000), 0.001, 100, 1, startphs=0.99, standard=False)
-assert(np.allclose(prof, prof.mean()))
-assert(np.all(prof>0))
+assert np.allclose(prof, prof.mean())
+assert np.all(prof > 0)
 print("success")
